4bac23436ab84a4efa2df061c92f98550e058baf
Morten Dahl <mortendahlcs@gmail.com>
2020-08-19 15:07:15 +0200

    ..

--- async-prototype/main.py
+++ async-prototype/main.py
@@ -58,6 +58,7 @@ if __name__ == "__main__":
         runtime = TestRuntime(num_workers=len(concrete_comp.devices()))
     elif args.runtime == "remote":
         runtime = RemoteRuntime("cluster/cluster-spec-localhost.yaml")
+        assert len(runtime.executors) == len(concrete_comp.devices())
     else:
         raise ValueError(f"Unknown runtime '{args.runtime}'")
 
