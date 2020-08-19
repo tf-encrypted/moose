6aaeb90af9da168487427f2bb5a3791d610a541c
yann dupis <yann.dupis@gmail.com>
2020-08-19 11:24:51 -0700

    working with rebase

--- async-prototype/main_run_script.py
+++ async-prototype/main_run_script.py
@@ -1,6 +1,5 @@
 import logging
 
-from channels import AsyncChannelManager
 from computation import Computation
 from edsl import Role
 from edsl import add
@@ -11,10 +10,10 @@ from edsl import load
 from edsl import mul
 from edsl import save
 from edsl import sub
-from executor import AsyncKernelBasedExecutor
 from logger import get_logger
 from logger import set_logger
-from runtime import Runtime
+from runtime import RemoteRuntime
+from runtime import TestRuntime
 
 get_logger().setLevel(level=logging.DEBUG)
 
@@ -44,21 +43,18 @@ def my_comp():
 
 concrete_comp = my_comp.trace_func()
 
-channel_manager = AsyncChannelManager()
+if __name__ == "__main__":
 
-in0_executor = AsyncKernelBasedExecutor(name="alice", channel_manager=channel_manager,)
-in1_executor = AsyncKernelBasedExecutor(name="bob", channel_manager=channel_manager,)
-agg_executor = AsyncKernelBasedExecutor(name="carole", channel_manager=channel_manager,)
-out_executor = AsyncKernelBasedExecutor(name="dave", channel_manager=channel_manager)
+    runtime = TestRuntime(num_workers=len(concrete_comp.devices()))
 
-runtime = Runtime(
-    role_assignment={
-        inputter0: in0_executor,
-        inputter1: in1_executor,
-        aggregator: agg_executor,
-        outputter: out_executor,
-    }
-)
+    runtime.evaluate_computation(
+        computation=concrete_comp,
+        role_assignment={
+            inputter0: runtime.executors[0],
+            inputter1: runtime.executors[1],
+            aggregator: runtime.executors[2],
+            outputter: runtime.executors[3],
+        },
+    )
 
-runtime.evaluate_computation(concrete_comp)
-print("Done")
+    print("Done")
