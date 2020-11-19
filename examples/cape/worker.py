2dc777b983d0cfdbe81eb8f29a8b0d8a1881a8fa
Morten Dahl <mortendahlcs@gmail.com>
2020-11-19 17:39:37 +0100

    Adapt example

--- examples/cape/worker.py
+++ examples/cape/worker.py
@@ -5,8 +5,8 @@ import os
 
 from grpc.experimental import aio as grpc_aio
 
-from moose.choreography.grpc import Choreography
-from moose.executor.executor import AsyncExecutor
+from moose.choreography.cape_coordinator import Choreography
+from moose.executor import AsyncExecutor
 from moose.logger import get_logger
 from moose.networking.cape_broker import Networking
 
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser(description="Launch worker")
 parser.add_argument("--port", type=int, default=50000)
 parser.add_argument("--name", default=os.environ.get("CAPE_NAME", None))
 parser.add_argument("--broker", default=os.environ.get("CAPE_BROKER", None))
+parser.add_argument("--coordinator", default=os.environ.get("CAPE_COORDINATOR", None))
 parser.add_argument("--token", default=os.environ.get("CAPE_TOKEN", None))
 parser.add_argument("--verbose", action="store_true")
 args = parser.parse_args()
@@ -30,14 +31,10 @@ if __name__ == "__main__":
         broker_host=args.broker, own_name=args.name, auth_token=args.token
     )
     executor = AsyncExecutor(networking=networking)
-
-    grpc_aio.init_grpc_aio()
-    grpc_server = grpc_aio.server()
-    grpc_server.add_insecure_port(f"0.0.0.0:{args.port}")
-    choreography = Choreography(executor=executor, grpc_server=grpc_server)
-
-    asyncio.get_event_loop().run_until_complete(grpc_server.start())
-    logger.info("Worker started")
-
-    asyncio.get_event_loop().run_until_complete(grpc_server.wait_for_termination())
-    logger.info("Worker stopped")
+    choreography = Choreography(
+        executor=executor,
+        coordinator_host=args.coordinator,
+        own_name=args.name,
+        auth_token=args.token,
+    )
+    asyncio.get_event_loop().run_until_complete(choreography.run())
