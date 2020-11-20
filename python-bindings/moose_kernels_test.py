a21958c6df7253df350b6e059aa527c059b38af0
jvmancuso <jason@manc.us>
2020-11-20 13:06:02 -0500

    fix python spacing

--- python-bindings/moose_kernels_test.py
+++ python-bindings/moose_kernels_test.py
@@ -1,10 +1,11 @@
 import numpy as np
 from moose_kernels import ring_add
 
-def test_ring_add_usual():
-  a = np.array([1,2,3], dtype=np.uint64)
-  b = np.array([4,5,6], dtype=np.uint64)
-  c1 = a + b
-  c2 = ring_add(a, b)
 
-  np.testing.assert_array_equal(c1, c2)
+def test_ring_add_usual():
+    a = np.array([1, 2, 3], dtype=np.uint64)
+    b = np.array([4, 5, 6], dtype=np.uint64)
+    c1 = a + b
+    c2 = ring_add(a, b)
+
+    np.testing.assert_array_equal(c1, c2)
