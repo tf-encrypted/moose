2e0c23b68ba58e4365c31f48f1736e261519d123
jvmancuso <jason@manc.us>
2020-12-01 14:31:21 -0500

    ring_mul implementation

--- python-bindings/moose_test.py
+++ python-bindings/moose_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from moose import ring_add
+from moose import ring_add, ring_mul
 
 
 def test_ring_add_usual():
@@ -7,5 +7,14 @@ def test_ring_add_usual():
     b = np.array([4, 5, 6], dtype=np.uint64)
     c1 = a + b
     c2 = ring_add(a, b)
+
+    np.testing.assert_array_equal(c1, c2)
+
+
+def test_ring_mul_usual():
+    a = np.array([1, 2, 3], dtype=np.uint64)
+    b = np.array([4, 5, 6], dtype=np.uint64)
+    c1 = a * b
+    c2 = ring_mul(a, b)
 
     np.testing.assert_array_equal(c1, c2)
