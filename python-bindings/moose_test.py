3c17f2087ff1ac6c6f709ffa900a2a86b080a4d5
jvmncs <jason@dropoutlabs.com>
2020-12-02 13:33:31 -0500

    ring_mul + ring_sub implementations (#99)
    * ring_mul implementation
    
    * ring_sub implementation
    
    * bump ndarray version

--- python-bindings/moose_test.py
+++ python-bindings/moose_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from moose import ring_add
+from moose import ring_add, ring_mul, ring_sub
 
 
 def test_ring_add_usual():
@@ -7,5 +7,23 @@ def test_ring_add_usual():
     b = np.array([4, 5, 6], dtype=np.uint64)
     c1 = a + b
     c2 = ring_add(a, b)
+
+    np.testing.assert_array_equal(c1, c2)
+
+
+def test_ring_mul_usual():
+    a = np.array([1, 2, 3], dtype=np.uint64)
+    b = np.array([4, 5, 6], dtype=np.uint64)
+    c1 = a * b
+    c2 = ring_mul(a, b)
+
+    np.testing.assert_array_equal(c1, c2)
+
+
+def test_ring_sub_usual():
+    a = np.array([1, 2, 3], dtype=np.uint64)
+    b = np.array([4, 5, 6], dtype=np.uint64)
+    c1 = b - a
+    c2 = ring_sub(b, a)
 
     np.testing.assert_array_equal(c1, c2)
