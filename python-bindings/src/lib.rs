3596715f980a37d37f13c045ae6e452a25830a58
jvmancuso <jason@manc.us>
2020-12-03 10:37:24 -0500

    address PR comments

--- python-bindings/src/lib.rs
+++ python-bindings/src/lib.rs
@@ -4,40 +4,40 @@ use numpy::{PyArrayDyn, PyReadonlyArrayDyn, ToPyArray};
 use pyo3::prelude::*;
 use std::num::Wrapping;
 
+fn dynarray_to_ring64(arr: &PyReadonlyArrayDyn<u64>) -> Ring64Tensor {
+    let arr_wrap = arr.as_array().mapv(Wrapping);
+    Ring64Tensor(arr_wrap)
+}
+
+fn ring64_to_array(r: Ring64Tensor, shape: &[usize]) -> ArrayD<u64> {
+    let inner_arr = r.0;
+    let unwrapped = inner_arr.mapv(|x| x.0);
+    unwrapped.into_shape(shape).unwrap()
+}
+
+fn binary_pyfn<'py>(
+    py: Python<'py>,
+    x: PyReadonlyArrayDyn<u64>,
+    y: PyReadonlyArrayDyn<u64>,
+    binary_op: impl Fn(Ring64Tensor, Ring64Tensor) -> Ring64Tensor,
+) -> &'py PyArrayDyn<u64> {
+    let x_shape = x.shape();
+    let x_ring = dynarray_to_ring64(&x);
+    let y_ring = dynarray_to_ring64(&y);
+    let res = binary_op(x_ring, y_ring);
+    let res_array = ring64_to_array(res, x_shape);
+    res_array.to_pyarray(py)
+}
+
 #[pymodule]
 fn moose_kernels(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    fn dynarray_to_ring64(arr: &PyReadonlyArrayDyn<u64>) -> Ring64Tensor {
-        let arr_wrap = arr.as_array().mapv(Wrapping);
-        Ring64Tensor(arr_wrap)
-    }
-
-    fn ring64_to_array(r: Ring64Tensor, shape: &[usize]) -> ArrayD<u64> {
-        let inner_arr = r.0;
-        let unwrapped = inner_arr.mapv(|x| x.0);
-        unwrapped.into_shape(shape).unwrap()
-    }
-
-    fn binary_pyfn<'py>(
-        py: Python<'py>,
-        x: PyReadonlyArrayDyn<u64>,
-        y: PyReadonlyArrayDyn<u64>,
-        binary_op: &dyn Fn(Ring64Tensor, Ring64Tensor) -> Ring64Tensor,
-    ) -> &'py PyArrayDyn<u64> {
-        let x_shape = x.shape();
-        let x_ring = dynarray_to_ring64(&x);
-        let y_ring = dynarray_to_ring64(&y);
-        let res = binary_op(x_ring, y_ring);
-        let res_array = ring64_to_array(res, x_shape);
-        res_array.to_pyarray(py)
-    }
-
     #[pyfn(m, "ring_add")]
     fn ring_add<'py>(
         py: Python<'py>,
         x: PyReadonlyArrayDyn<u64>,
         y: PyReadonlyArrayDyn<u64>,
     ) -> &'py PyArrayDyn<u64> {
-        binary_pyfn(py, x, y, &|a, b| a + b)
+        binary_pyfn(py, x, y, |a, b| a + b)
     }
 
     #[pyfn(m, "ring_mul")]
@@ -46,7 +46,7 @@ fn moose_kernels(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         x: PyReadonlyArrayDyn<u64>,
         y: PyReadonlyArrayDyn<u64>,
     ) -> &'py PyArrayDyn<u64> {
-        binary_pyfn(py, x, y, &|a, b| a * b)
+        binary_pyfn(py, x, y, |a, b| a * b)
     }
 
     #[pyfn(m, "ring_sub")]
@@ -55,7 +55,7 @@ fn moose_kernels(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         x: PyReadonlyArrayDyn<u64>,
         y: PyReadonlyArrayDyn<u64>,
     ) -> &'py PyArrayDyn<u64> {
-        binary_pyfn(py, x, y, &|a, b| a - b)
+        binary_pyfn(py, x, y, |a, b| a - b)
     }
 
     Ok(())
