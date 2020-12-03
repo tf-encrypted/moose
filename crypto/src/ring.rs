88136516466c37ca4e1e64fa215e79c1f45dd3e9
Dragoș Rotaru <rdragos@users.noreply.github.com>
2020-12-03 16:04:35 +0200

    [nextgen] AES PRNG (#106)
    * adding stub
    
    * ChaCha20 sampling for Ring64Vector
    
    * something that compiles
    
    * another draft
    
    * save state
    
    * something that compiles. now need to benchmark
    
    * ran fmt
    
    * refactoring
    
    * something drafty
    
    * copying stuff around
    
    * first steps towards benchmarking
    
    * ready to bench
    
    * added fast aes based prng
    
    * Suggestions for nextgen-aes (#97)
    
    * Get rid of AesRngSeed
    
    * ..
    
    * Git rid of Hash trait
    
    * Remove magic numbers
    
    * Reduce iterations in benchmark
    
    * Keep criterion as dev dependency only
    
    * Small up refactor of benches
    
    * Compile with +aes target feature by default
    
    * Renaming
    
    * added tests and seeded prng
    
    * linting
    
    * added PR comments
    
    * added documentation flag and fmt code
    
    * added documentation
    
    * added more verbose tests
    
    * fixed doc string
    
    Co-authored-by: Morten Dahl <mortendahlcs@gmail.com>

---
+++ crypto/src/ring.rs
@@ -0,0 +1,68 @@
+use ndarray::prelude::*;
+use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
+use std::num::Wrapping;
+use std::ops::{Add, Mul};
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Ring64Vector(Array1<Wrapping<u64>>);
+
+pub trait Sample {
+    fn sample_uniform(size: usize) -> Self;
+}
+
+impl Sample for Ring64Vector {
+    fn sample_uniform(size: usize) -> Self {
+        let mut rng = ChaCha20Rng::seed_from_u64(42);
+        let values: Vec<_> = (0..size).map(|_| Wrapping(rng.next_u64())).collect();
+        Ring64Vector(Array1::from(values))
+    }
+}
+
+impl From<Vec<u64>> for Ring64Vector {
+    fn from(v: Vec<u64>) -> Ring64Vector {
+        use vec_utils::VecExt;
+        let v_wrapped: Vec<_> = v.map(Wrapping);
+        Ring64Vector(Array1::from(v_wrapped))
+    }
+}
+
+impl From<&[u64]> for Ring64Vector {
+    fn from(v: &[u64]) -> Ring64Vector {
+        let v_wrapped: Vec<_> = v.iter().map(|vi| Wrapping(*vi)).collect();
+        Ring64Vector(Array1::from(v_wrapped))
+    }
+}
+
+impl Add<Ring64Vector> for Ring64Vector {
+    type Output = Ring64Vector;
+    fn add(self, other: Ring64Vector) -> Self::Output {
+        Ring64Vector(self.0.add(other.0))
+    }
+}
+
+impl Mul<Ring64Vector> for Ring64Vector {
+    type Output = Ring64Vector;
+    fn mul(self, other: Ring64Vector) -> Self::Output {
+        Ring64Vector(self.0.mul(other.0))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ring_mul() {
+        let a = Ring64Vector::from(vec![1, 2, 3]);
+        let b = Ring64Vector::from(vec![4, 5, 6]);
+        let c = a * b;
+        assert_eq!(c, Ring64Vector::from(vec![4, 10, 18]));
+    }
+
+    #[test]
+    fn ring_sample() {
+        let r = Ring64Vector::sample_uniform(5);
+        assert_eq!(r, Ring64Vector::from(vec![4, 10, 18]));
+    }
+}
