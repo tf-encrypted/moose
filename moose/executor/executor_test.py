93d5b7cfbd681e936617a5f0daa510081d6c645d
Morten Dahl <mortendahlcs@gmail.com>
2020-12-08 11:28:57 +0100

    Add support for (async) computation arguments

--- moose/executor/executor_test.py
+++ moose/executor/executor_test.py
@@ -1,9 +1,13 @@
+import asyncio
 import logging
 import pathlib
 import unittest
 
 from absl.testing import parameterized
 
+from moose.computation import standard as standard_ops
+from moose.computation.base import Computation
+from moose.computation.host import HostPlacement
 from moose.edsl.base import add
 from moose.edsl.base import computation
 from moose.edsl.base import constant
@@ -15,6 +19,7 @@ from moose.edsl.base import run_program
 from moose.edsl.base import save
 from moose.edsl.base import sub
 from moose.edsl.tracer import trace
+from moose.executor.executor import AsyncExecutor
 from moose.logger import get_logger
 from moose.runtime import TestRuntime as Runtime
 
@@ -63,6 +68,39 @@ class ExecutorTest(parameterized.TestCase):
 
         comp_result = _run_computation(my_comp, [player0, player1])
         self.assertEqual(comp_result["result"], 5)
+
+    def test_input(self):
+        comp = Computation(operations={}, placements={})
+
+        alice = comp.add_placement(HostPlacement(name="alice"))
+
+        comp.add_operation(
+            standard_ops.InputOperation(name="x", placement_name=alice.name, inputs={})
+        )
+        comp.add_operation(
+            standard_ops.InputOperation(name="y", placement_name=alice.name, inputs={})
+        )
+        comp.add_operation(
+            standard_ops.AddOperation(
+                name="add", placement_name=alice.name, inputs={"lhs": "x", "rhs": "y"}
+            )
+        )
+        comp.add_operation(
+            standard_ops.SaveOperation(
+                name="save", placement_name=alice.name, inputs={"value": "add"}, key="z"
+            )
+        )
+
+        executor = AsyncExecutor(networking=None)
+        task = executor.run_computation(
+            comp,
+            placement_instantiation={alice.name: alice.name},
+            placement=alice.name,
+            session_id="0123456789",
+            arguments={"x": 5, "y": 10},
+        )
+        asyncio.get_event_loop().run_until_complete(task)
+        assert executor.store["z"] == 15
 
     @parameterized.parameters(
         {"op": op, "expected_result": expected_result}
