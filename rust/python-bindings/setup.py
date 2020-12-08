fe960e6520d28dc77c28d21782385a48772e727a
jvmancuso <jason@manc.us>
2020-12-08 11:49:53 -0500

    format python bindings for rust kernels, fix up req files

--- rust/python-bindings/setup.py
+++ rust/python-bindings/setup.py
@@ -1,19 +1,16 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 from setuptools_rust import RustExtension
 
-
-setup_requires = ['setuptools-rust~=0.11.5']
-install_requires = ['numpy']
-test_requires = install_requires + ['pytest', 'absl-py']
+setup_requires = ["setuptools-rust~=0.11.5"]
+install_requires = ["numpy"]
+test_requires = install_requires + ["pytest", "absl-py"]
 
 setup(
-    name='moose_kernels',
-    version='0.1.0',
-    description='Example of python extension using rust-numpy',
-    rust_extensions=[RustExtension(
-        'moose_kernels.moose_kernels',
-        './Cargo.toml',
-    )],
+    name="moose_kernels",
+    version="0.1.0",
+    description="Example of python extension using rust-numpy",
+    rust_extensions=[RustExtension("moose_kernels.moose_kernels", "./Cargo.toml",)],
     install_requires=install_requires,
     setup_requires=setup_requires,
     test_requires=test_requires,
