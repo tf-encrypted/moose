fab7eabcbfd5e48698601d4df2953f3974d5bc8f
jvmancuso <jason@manc.us>
2020-12-16 17:49:13 -0500

    introduce RingDotKernel and RingDotOperation

--- moose/executor/kernels/ring.py
+++ moose/executor/kernels/ring.py
@@ -1,9 +1,11 @@
 from moose_kernels import ring_add
+from moose_kernels import ring_dot
 from moose_kernels import ring_mul
 from moose_kernels import ring_shape
 from moose_kernels import ring_sub
 
 from moose.computation.ring import RingAddOperation
+from moose.computation.ring import RingDotOperation
 from moose.computation.ring import RingMulOperation
 from moose.computation.ring import RingShapeOperation
 from moose.computation.ring import RingSubOperation
@@ -20,6 +22,12 @@ class RingMulKernel(Kernel):
     def execute_synchronous_block(self, op, session, lhs, rhs):
         assert isinstance(op, RingMulOperation)
         return ring_mul(lhs, rhs)
+
+
+class RingDotKernel(Kernel):
+    def execute_synchronous_block(self, op, session, lhs, rhs):
+        assert isinstance(op, RingDotOperation)
+        return ring_dot(lhs, rhs)
 
 
 class RingSubKernel(Kernel):
