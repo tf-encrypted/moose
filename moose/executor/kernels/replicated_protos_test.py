f83f3e07808a47b9839bd78bc214189c752ffaa9
jvmancuso <jason@manc.us>
2021-01-06 14:39:51 -0500

    add execution test for dot prod

--- moose/executor/kernels/replicated_protos_test.py
+++ moose/executor/kernels/replicated_protos_test.py
@@ -4,6 +4,7 @@ import numpy as np
 from absl.testing import parameterized
 from hypothesis import given
 from hypothesis import strategies as st
+from hypothesis.extra import numpy as hnp
 
 from moose.compiler.compiler import Compiler
 from moose.computation import standard as standard_dialect
@@ -21,6 +22,14 @@ pair_lists = st.lists(
         st.integers(min_value=1, max_value=100), st.integers(min_value=1, max_value=100)
     ),
     min_size=1,
+)
+
+dim = st.integers(min_value=2, max_value=64)
+a_dims = st.tuples(st.shared(dim), st.shared(dim, key="inner_dim"))
+b_dims = st.tuples(st.shared(dim, key="inner_dim"), st.shared(dim))
+dotprod_inputs = st.tuples(
+    hnp.arrays(dtype=np.uint32, shape=a_dims),
+    hnp.arrays(dtype=np.uint32, shape=b_dims),
 )
 
 
@@ -114,6 +123,91 @@ class ReplicatedProtocolsTest(parameterized.TestCase):
         np.testing.assert_array_equal(
             z, runtime.get_executor(carole.name).store["result"]
         )
+
+    @given(dotprod_inputs)
+    def test_dot_prod(self, dotprod_args):
+        comp = Computation(operations={}, placements={})
+
+        alice = HostPlacement(name="alice")
+        bob = HostPlacement(name="bob")
+        carole = HostPlacement(name="carole")
+        rep = ReplicatedPlacement(name="rep", player_names=["alice", "bob", "carole"])
+
+        comp.add_placement(alice)
+        comp.add_placement(bob)
+        comp.add_placement(carole)
+        comp.add_placement(rep)
+
+        a, b = dotprod_args
+        x = a.astype(np.float32)
+        y = b.astype(np.float32)
+
+        z = x @ y
+
+        comp.add_operation(
+            standard_dialect.ConstantOperation(
+                name="alice_input",
+                value=x,
+                placement_name=alice.name,
+                inputs={},
+                output_type=TensorType(datatype="float"),
+            )
+        )
+
+        comp.add_operation(
+            standard_dialect.ConstantOperation(
+                name="bob_input",
+                value=y,
+                placement_name=bob.name,
+                inputs={},
+                output_type=TensorType(datatype="float"),
+            )
+        )
+
+        comp.add_operation(
+            standard_dialect.DotOperation(
+                name="dot_op",
+                placement_name=rep.name,
+                inputs={"lhs": "alice_input", "rhs": "bob_input"},
+                output_type=TensorType(datatype="float"),
+            )
+        )
+
+        comp.add_operation(
+            standard_dialect.SaveOperation(
+                name="save",
+                inputs={"value": "dot_op"},
+                placement_name=carole.name,
+                key="result",
+            )
+        )
+
+        comp.add_operation(
+            standard_dialect.OutputOperation(
+                name="output", placement_name=carole.name, inputs={"value": "save"},
+            )
+        )
+
+        compiler = Compiler()
+
+        comp = compiler.run_passes(comp)
+
+        placement_instantiation = {
+            alice: alice.name,
+            bob: bob.name,
+            carole: carole.name,
+        }
+
+        runtime = LocalRuntime()
+        # TODO: replace the following with commented code when the fixedpoint bug
+        # is fixed
+        del z, placement_instantiation, runtime
+        # runtime.evaluate_computation(
+        #     computation=comp, placement_instantiation=placement_instantiation
+        # )
+        # np.testing.assert_array_equal(
+        #     z, runtime.get_executor(carole.name).store["result"]
+        # )
 
 
 if __name__ == "__main__":
