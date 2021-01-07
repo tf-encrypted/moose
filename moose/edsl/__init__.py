151adf698eda629599b000179c4b0eb9d3a3bdc1
yanndupis <yann.dupis@gmail.com>
2021-01-07 15:44:12 -0800

    add concatenate, inverse & ones to edsl __init__ file

--- moose/edsl/__init__.py
+++ moose/edsl/__init__.py
@@ -1,13 +1,16 @@
 from moose.edsl.base import add
 from moose.edsl.base import computation
+from moose.edsl.base import concatenate
 from moose.edsl.base import constant
 from moose.edsl.base import div
 from moose.edsl.base import dot
 from moose.edsl.base import function
 from moose.edsl.base import host_placement
+from moose.edsl.base import inverse
 from moose.edsl.base import load
 from moose.edsl.base import mpspdz_placement
 from moose.edsl.base import mul
+from moose.edsl.base import ones
 from moose.edsl.base import replicated_placement
 from moose.edsl.base import run_program
 from moose.edsl.base import save
@@ -17,6 +20,7 @@ from moose.edsl.tracer import trace
 __all__ = [
     add,
     computation,
+    concatenate,
     constant,
     host_placement,
     mpspdz_placement,
@@ -24,8 +28,10 @@ __all__ = [
     div,
     dot,
     function,
+    inverse,
     load,
     mul,
+    ones,
     run_program,
     save,
     sub,
