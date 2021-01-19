993e8022f7e56f073e36aa1e7b473a0ff3353111
Jason Mancuso <jason@manc.us>
2021-01-19 12:35:48 -0500

    preliminary fixedpoint::ring_mean implementation for Ring64Tensor

--- rust/crypto/src/fixedpoint.rs
+++ rust/crypto/src/fixedpoint.rs
@@ -1,5 +1,6 @@
 use crate::ring::Ring64Tensor;
 use ndarray::prelude::*;
+use std::ops::Mul;
 
 pub fn ring_encode(x: &ArrayViewD<f64>, scaling_factor: u64) -> Ring64Tensor {
     let x_upshifted = x * scaling_factor as f64;
@@ -13,8 +14,26 @@ pub fn ring_decode(x: &Ring64Tensor, scaling_factor: u64) -> ArrayD<f64> {
     x_converted / scaling_factor as f64
 }
 
-pub fn ring_mean(x: &Ring64Tensor, axis: Option<u64>) -> Ring64Tensor {
-    
+pub fn ring_mean(x: Ring64Tensor, axis: Option<usize>, scaling_factor: u64) -> Ring64Tensor {
+    let shape: &[usize] = x.0.shape();
+    if let Some(ax) = axis {
+        let dim_len = shape[ax] as f64;
+        let mean_weight = Array::from_elem([], 1.0 / dim_len)
+            .into_dimensionality::<IxDyn>()
+            .unwrap();
+        let encoded_weight = ring_encode(&mean_weight.view(), scaling_factor);
+        let operand_sum = x.sum(axis);
+        operand_sum.mul(encoded_weight)
+    } else {
+        let dim_prod: usize = std::iter::Product::product(shape.iter());
+        let dim_prod = dim_prod as f64;
+        let mean_weight = Array::from_elem([], dim_prod)
+            .into_dimensionality::<IxDyn>()
+            .unwrap();
+        let encoded_weight = ring_encode(&mean_weight.view(), scaling_factor);
+        let operand_sum = x.sum(None);
+        operand_sum.mul(encoded_weight)
+    }
 }
 
 #[cfg(test)]
@@ -41,5 +60,17 @@ mod tests {
 
         let x_decoded = ring_decode(&x_encoded, scaling_factor);
         assert_eq!(x_decoded, x);
+    }
+
+    #[test]
+    fn ring_mean_with_axis() {
+        let x_backing: ArrayD<f64> = array![[1., 2.], [3., 4.]]
+            .into_dimensionality::<IxDyn>()
+            .unwrap();
+        let scaling_factor = 2u64.pow(16);
+        let x = ring_encode(&x_backing.view(), scaling_factor);
+        let out = ring_mean(x, Some(0), scaling_factor);
+        let dec = ring_decode(&out, scaling_factor);
+        assert_eq!(dec, array![2., 3.].into_dimensionality::<IxDyn>().unwrap());
     }
 }
