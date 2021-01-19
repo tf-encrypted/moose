b804a3c69fe3dda8a65fb1c09e011d2aae43dbad
Morten Dahl <mortendahlcs@gmail.com>
2021-01-19 17:04:30 +0100

    ..

--- moose/executor/kernels/host.py
+++ moose/executor/kernels/host.py
@@ -9,44 +9,48 @@ from moose.computation.host import CallPythonFunctionOperation
 from moose.computation.host import RunProgramOperation
 from moose.executor.kernels.base import Kernel
 from moose.logger import get_logger
+from moose.logger import get_tracer
 
 
 class CallPythonFunctionKernel(Kernel):
     async def execute(self, op, session, output, **inputs):
         assert isinstance(op, CallPythonFunctionOperation)
-        python_fn = dill.loads(op.pickled_fn)
         concrete_inputs = await asyncio.gather(*inputs.values())
-        concrete_output = python_fn(*concrete_inputs)
-        output.set_result(concrete_output)
+        with get_tracer().start_as_current_span(f"{op.name}"):
+            python_fn = dill.loads(op.pickled_fn)
+            concrete_output = python_fn(*concrete_inputs)
+            output.set_result(concrete_output)
 
 
 class RunProgramKernel(Kernel):
     async def execute(self, op, session, output, **inputs):
         assert isinstance(op, RunProgramOperation)
-        with tempfile.NamedTemporaryFile() as inputfile:
-            with tempfile.NamedTemporaryFile() as outputfile:
+        concrete_inputs = await asyncio.gather(*inputs.values())
+        with get_tracer().start_as_current_span(f"{op.name}"):
 
-                concrete_inputs = await asyncio.gather(*inputs.values())
-                inputfile.write(json.dumps(concrete_inputs).encode())
-                inputfile.flush()
+            with tempfile.NamedTemporaryFile() as inputfile:
+                with tempfile.NamedTemporaryFile() as outputfile:
 
-                args = [
-                    op.path,
-                    *op.args,
-                    "--input-file",
-                    inputfile.name,
-                    "--output-file",
-                    outputfile.name,
-                    "--session-id",
-                    str(session.session_id),
-                    "--placement",
-                    op.placement_name,
-                ]
-                get_logger().debug(f"Running external program: {args}")
-                _ = subprocess.run(
-                    args, stdout=subprocess.PIPE, universal_newlines=True,
-                )
+                    inputfile.write(json.dumps(concrete_inputs).encode())
+                    inputfile.flush()
 
-                concrete_output = json.loads(outputfile.read())
+                    args = [
+                        op.path,
+                        *op.args,
+                        "--input-file",
+                        inputfile.name,
+                        "--output-file",
+                        outputfile.name,
+                        "--session-id",
+                        str(session.session_id),
+                        "--placement",
+                        op.placement_name,
+                    ]
+                    get_logger().debug(f"Running external program: {args}")
+                    _ = subprocess.run(
+                        args, stdout=subprocess.PIPE, universal_newlines=True,
+                    )
 
-        output.set_result(concrete_output)
+                    concrete_output = json.loads(outputfile.read())
+
+            output.set_result(concrete_output)
