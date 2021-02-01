fe56fbce4b771f340adcf217eb1f3c5a2455dee1
Morten Dahl <mortendahlcs@gmail.com>
2021-02-01 16:03:34 +0100

    remove metrics for networking

--- moose/networking/memory.py
+++ moose/networking/memory.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 
-from moose.logger import get_meter
 from moose.utils import AsyncStore
 
 
@@ -20,35 +19,18 @@ class Channel:
 class Networking:
     def __init__(self):
         self.channels = defaultdict(Channel)
-        self.sent_counter = get_meter().create_counter(
-            name="bytes_sent",
-            description="number of bytes sent",
-            unit="bytes",
-            value_type=int,
-        )
-        self.received_counter = get_meter().create_counter(
-            name="bytes_received",
-            description="number of bytes received",
-            unit="bytes",
-            value_type=int,
-        )
 
     def get_hostname(self, placement):
         return "localhost"
 
     async def receive(self, sender, receiver, rendezvous_key, session_id):
         channel_key = (sender, receiver)
-        value = await self.channels[channel_key].receive(
+        return await self.channels[channel_key].receive(
             rendezvous_key=rendezvous_key, session_id=session_id
         )
-        self.received_counter.add(
-            len(value) * 8, labels={"moose.session_id": session_id}
-        )
-        return value
 
     async def send(self, value, sender, receiver, rendezvous_key, session_id):
         channel_key = (sender, receiver)
-        self.sent_counter.add(len(value) * 8, labels={"moose.session_id": session_id})
         await self.channels[channel_key].send(
             value, rendezvous_key=rendezvous_key, session_id=session_id
         )
