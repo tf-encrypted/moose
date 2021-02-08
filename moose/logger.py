b3fd88827331b9a2c29599a30e4c04c8254314e9
Morten Dahl <mortendahlcs@gmail.com>
2021-02-08 09:49:47 +0100

    cleanup

--- moose/logger.py
+++ moose/logger.py
@@ -20,15 +20,3 @@ logging.basicConfig()
 
 def get_tracer():
     return trace.get_tracer(__name__)
-
-
-_METER = None
-
-
-def set_meter(meter):
-    global _METER
-    _METER = meter
-
-
-def get_meter():
-    return _METER
