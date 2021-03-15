e71fc9aea6926aab9550668def6dbecde05a0e2f
jvmancuso <jason@manc.us>
2021-03-15 12:59:19 -0400

    add mean lowering to fp HostLoweringPass

--- moose/compiler/fixedpoint/host_lowering_pass_test.py
+++ moose/compiler/fixedpoint/host_lowering_pass_test.py
@@ -1,5 +1,6 @@
 import unittest
 
+import numpy as np
 from absl.testing import parameterized
 
 from moose.compiler.compiler import Compiler
@@ -144,6 +145,107 @@ class HostLoweringPassTest(parameterized.TestCase):
                 placement_name="alice",
             )
         )
+        assert comp.operations == expected_comp.operations
+        assert comp == expected_comp
+
+    @parameterized.parameters(None, 0, 1)
+    def test_mean_lowering(self, axis):
+        comp = Computation(placements={}, operations={})
+
+        comp.add_placement(HostPlacement(name="alice"))
+        comp.add_operation(
+            standard_ops.ConstantOperation(
+                name="x_input",
+                inputs={},
+                value=np.array([[2.0, 4.0], [6.0, 8.0]], dtype=np.float64),
+                placement_name="alice",
+                output_type=TensorType(dtype=dtypes.float64),
+            )
+        )
+        comp.add_operation(
+            fixedpoint_ops.EncodeOperation(
+                name="x_encode",
+                placement_name="alice",
+                inputs={"value": "x_input"},
+                output_type=fixedpoint_ops.EncodedTensorType(
+                    dtype=dtypes.fixed(14, 23), precision=23,
+                ),
+                precision=23,
+            )
+        )
+        comp.add_operation(
+            standard_ops.MeanOperation(
+                name="x_mean",
+                placement_name="alice",
+                inputs={"value": "x_encode"},
+                axis=axis,
+                output_type=TensorType(dtypes.fixed(14, 23)),
+            )
+        )
+        comp.add_operation(
+            standard_ops.OutputOperation(
+                name="output_0", inputs={"value": "x_mean"}, placement_name="alice",
+            )
+        )
+
+        compiler = Compiler(passes=[host_lowering_pass.HostLoweringPass()])
+        comp = compiler.run_passes(comp)
+
+        expected_comp = Computation(placements={}, operations={})
+        expected_comp.add_placement(HostPlacement(name="alice"))
+        expected_comp.add_operation(
+            standard_ops.ConstantOperation(
+                name="x_input",
+                inputs={},
+                value=np.array([[2.0, 4.0], [6.0, 8.0]], dtype=np.float64),
+                placement_name="alice",
+                output_type=TensorType(dtype=dtypes.float64),
+            )
+        )
+        expected_comp.add_operation(
+            fixedpoint_ops.EncodeOperation(
+                name="x_encode",
+                placement_name="alice",
+                inputs={"value": "x_input"},
+                output_type=fixedpoint_ops.EncodedTensorType(
+                    dtype=dtypes.fixed(14, 23), precision=23,
+                ),
+                precision=23,
+            )
+        )
+        expected_comp.add_operation(
+            fixedpoint_ops.RingMeanOperation(
+                name="fixed_ring_mean_0",
+                placement_name="alice",
+                inputs={"value": "x_encode"},
+                output_type=fixedpoint_ops.EncodedTensorType(
+                    dtype=dtypes.fixed(14, 46), precision=46,
+                ),
+                axis=axis,
+                precision=23,
+            )
+        )
+        expected_comp.add_operation(
+            fixedpoint_ops.TruncOperation(
+                name="fixed_trunc_0",
+                placement_name="alice",
+                inputs={"value": "fixed_ring_mean_0"},
+                output_type=fixedpoint_ops.EncodedTensorType(
+                    dtype=dtypes.fixed(14, 23), precision=23,
+                ),
+                precision=23,
+            )
+        )
+        expected_comp.add_operation(
+            standard_ops.OutputOperation(
+                name="output_0",
+                inputs={"value": "fixed_trunc_0"},
+                placement_name="alice",
+            )
+        )
+        comp_const = comp.operations.pop("x_input")
+        expected_comp_const = expected_comp.operations.pop("x_input")
+        np.testing.assert_equal(comp_const.value, expected_comp_const.value)
         assert comp.operations == expected_comp.operations
         assert comp == expected_comp
 
