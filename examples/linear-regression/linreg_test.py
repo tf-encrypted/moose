725e3e2fbd660787996523af4c73fd70279e85b1
Dragos Rotaru <r.dragos0@gmail.com>
2021-05-11 15:19:03 +0300

    added build for mape

--- examples/linear-regression/linreg_test.py
+++ examples/linear-regression/linreg_test.py
@@ -27,6 +27,10 @@ def mse(y_pred, y_true):
     return edsl.mean(edsl.square(edsl.sub(y_pred, y_true)), axis=0)
 
 
+def mape(y_pred, y_true, y_true_inv):
+    return edsl.abs(edsl.mul(edsl.sub(y_pred, y_true), y_true_inv))
+
+
 def ss_res(y_pred, y_true):
     squared_residuals = edsl.square(edsl.sub(y_true, y_pred))
     return edsl.sum(squared_residuals, axis=0)
@@ -44,7 +48,7 @@ def r_squared(ss_res, ss_tot):
 
 
 class LinearRegressionExample(parameterized.TestCase):
-    def _build_linear_regression_example(self, compiler_passes=None):
+    def _build_linear_regression_with_mse_example(self, compiler_passes=None):
         x_owner = edsl.host_placement(name="x-owner")
         y_owner = edsl.host_placement(name="y-owner")
         model_owner = edsl.host_placement(name="model-owner")
@@ -107,8 +111,72 @@ class LinearRegressionExample(parameterized.TestCase):
         concrete_comp = edsl.trace_and_compile(my_comp, compiler_passes=compiler_passes)
         return (my_comp, concrete_comp), (x_owner, y_owner, model_owner, replicated_plc)
 
+    def _build_linear_regression_with_mape_example(self, compiler_passes=None):
+        x_owner = edsl.host_placement(name="x-owner")
+        y_owner = edsl.host_placement(name="y-owner")
+        model_owner = edsl.host_placement(name="model-owner")
+        replicated_plc = edsl.replicated_placement(
+            players=[x_owner, y_owner, model_owner], name="replicated-plc"
+        )
+
+        @edsl.computation
+        def my_comp(
+            x_uri: edsl.Argument(placement=x_owner, vtype=StringType()),
+            y_uri: edsl.Argument(placement=y_owner, vtype=StringType()),
+            w_uri: edsl.Argument(placement=model_owner, vtype=StringType()),
+            mape_uri: edsl.Argument(placement=model_owner, vtype=StringType()),
+            rsquared_uri: edsl.Argument(placement=model_owner, vtype=StringType()),
+        ):
+
+            with x_owner:
+                X = edsl.atleast_2d(
+                    edsl.load(x_uri, dtype=edsl.float32), to_column_vector=True
+                )
+                # NOTE: what would be most natural to do is this:
+                #     bias_shape = (slice(shape(X), begin=0, end=1), 1)
+                #     bias = ones(bias_shape, dtype=float)
+                # but this raises an issue about accomodating python native values in
+                # the ASTTracer, something we've discussed and temporarily tabled in
+                # the past. For now, we've decided to implement squeeze and unsqueeze
+                # ops instead.
+                # But we have a feeling this issue will continue to come up!
+                bias_shape = edsl.slice(edsl.shape(X), begin=0, end=1)
+                bias = edsl.ones(bias_shape, dtype=edsl.float32)
+                reshaped_bias = edsl.expand_dims(bias, 1)
+                X_b = edsl.concatenate([reshaped_bias, X], axis=1)
+                A = edsl.inverse(edsl.dot(edsl.transpose(X_b), X_b))
+                B = edsl.dot(A, edsl.transpose(X_b))
+
+            with y_owner:
+                y_true = edsl.atleast_2d(
+                    edsl.load(y_uri, dtype=edsl.float32), to_column_vector=True
+                )
+                y_true_inv = edsl.div(edsl.constant(1.0, dtype=edsl.float32), y_true)
+                totals_ss = ss_tot(y_true)
+
+            with replicated_plc:
+                w = edsl.dot(B, y_true)
+                y_pred = edsl.dot(X_b, w)
+                mape_result = mape(y_pred, y_true, y_true_inv)
+                residuals_ss = ss_res(y_pred, y_true)
+
+            with model_owner:
+                rsquared_result = r_squared(residuals_ss, totals_ss)
+
+            with model_owner:
+                res = (
+                    edsl.save(w_uri, w),
+                    edsl.save(mape_uri, mape_result),
+                    edsl.save(rsquared_uri, rsquared_result),
+                )
+
+            return res
+
+        concrete_comp = edsl.trace_and_compile(my_comp, compiler_passes=compiler_passes)
+        return (my_comp, concrete_comp), (x_owner, y_owner, model_owner, replicated_plc)
+
     def test_linear_regression_eval(self):
-        (_, concrete_comp), placements = self._build_linear_regression_example()
+        (_, concrete_comp), placements = self._build_linear_regression_with_mse_example()
         x_owner, y_owner, model_owner, replicated_plc = placements
 
         x_data, y_data = generate_data(seed=42, n_instances=10, n_features=1)
@@ -136,7 +204,7 @@ class LinearRegressionExample(parameterized.TestCase):
                 "x_uri": "x_data",
                 "y_uri": "y_data",
                 "w_uri": "regression_weights",
-                "mse_uri": "mse_result",
+                "metric_uri": "metric_result",
                 "rsquared_uri": "rsquared_result",
             },
         )
@@ -146,7 +214,7 @@ class LinearRegressionExample(parameterized.TestCase):
     @parameterized.parameters(True, False)
     def test_linear_regression_serde(self, compiled):
         passes_arg = None if compiled else []
-        (_, traced_comp), placements = self._build_linear_regression_example(
+        (_, traced_comp), placements = self._build_linear_regression_with_mse_example(
             compiler_passes=passes_arg
         )
         serialized = serialize_computation(traced_comp)
