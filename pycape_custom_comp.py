bd95fd422db5b973e8a732af38d67a988777a7c5
jvmancuso <jason@manc.us>
2021-05-20 18:14:24 -0400

    update stash

--- pycape_custom_comp.py
+++ pycape_custom_comp.py
@@ -1,5 +1,11 @@
 import abc
+import functools
+import inspect
+from typing import Generic, Iterable
+from typing import List
 from typing import NewType
+from typing import Optional
+from typing import TypeVar
 from typing import Tuple
 
 import numpy as np
@@ -12,16 +18,14 @@ from moose.computation.standard import StringType
 from moose.edsl.base import AbstractComputation
 from moose.testing import TestRuntime
 
-
-
 class Result:
-  def __init__(self, placement, named=None):
+  def __init__(self, placement, name: Optional[str] = None):
     self.placement = placement
-    self.name = named or "output"
+    self.name = name
 
-Result = NewType("Result", Result)
 
 setattr(edsl, "Result", Result)
+
 ##### GOALS #####
 # 1. Enable more than 1 computation
 # 2. Enable custom Moose computations for quick Cape dev experimentation
@@ -33,18 +37,16 @@ setattr(edsl, "Result", Result)
 
 ##### NOTES #####
 
-# mul = MultiplicationTask("alice", "bob", secret=False)  # x * y = z
-# z = mul.run(x=np.array([1.0]), y=np.array([2.0]))
-# print(z)  # np.array([3.0])
+mul = MultiplicationTask("alice", "bob", secret=False)  # x * y = z
+z = mul.run(x=np.array([1.0]), y=np.array([2.0]))
+print(z)  # np.array([3.0])
 
-# mul = MultiplicationTask("alice", "bob", secret=False)  # x * y = z
-# mul.seed_storage("alice", {"x": np.array([1.0])})
-# mul.seed_storage("bob", {"y": np.array([2.0])})
-# mul.run(x_key="x", y_key="y", out_key="result")
-# print(mul.get_results())
-# # result: np.array([3.0])
-
-# print(z)  # np.array([3.0])
+mul = MultiplicationTask("alice", "bob", secret=False)  # x * y = z
+mul.seed_storage("alice", {"x": np.array([1.0])})
+mul.seed_storage("bob", {"y": np.array([2.0])})
+mul.run(x_key="x", y_key="y", out_key="result")
+print(mul.get_results())
+# result: np.array([3.0])
 
 ###### EDSL FUNCTION DESIGN OPTIONS ######
 # All of these options refer to equivalent computations
@@ -74,15 +76,18 @@ def decorated(
 
 ## Option 1b: signature-driven
 # @place_outputs
+# @place_computation
 @edsl.computation
 def other(
   x: edsl.Argument(alice, dtype=edsl.float32),
   y: edsl.Argument(bob, dtype=edsl.float32),
-) -> Tuple[
-  edsl.Result(bob, named="bobs_x_result"),
-  edsl.Result(alice),
-  Tuple[edsl.Result(alice), edsl.Result(bob)],
-]:
+) -> tuple(
+    (
+      edsl.Result(bob, name="bobs_x_result"),
+      edsl.Result(alice),
+      (edsl.Result(alice), edsl.Result(bob)),
+    )
+):
   with multiply_placement:
     z = edsl.mul(x, y)
   return x, y, z
@@ -98,7 +103,7 @@ def completed(
 
   with multiply_placement:
     z = edsl.mul(x, y)
-  
+
   with bob:
     res_x = edsl.save("bobs_x_result", x)
     res_z = edsl.save("output2_1", z)
@@ -109,14 +114,69 @@ def completed(
   return res_x, res_y, res_z, res_z_alice
 
 
+def place_computation(abstract_computation=None, output_placements=None):
+  if abstract_computation is None and output_placements is None:
+    raise ValueError
+  elif abstract_computation is None and output_placements is not None:
+    return functools.partial(place_computation, output_placements=output_placements)
 
+  edsl_func = abstract_computation.func
+  edsl_sig = inspect.signature(edsl_func)
+  parameters = [p for p in edsl_sig.parameters.values()]
+  if edsl_sig.return_annotation != inspect.Signature.empty:
+    updated_annotation = []
+    for i, result in enumerate(edsl_sig.return_annotation):
+      if isinstance(result, Result):
+        if result.name is None:
+          updated_annotation.append(Result(result.placement, f"output{i}"))
+      elif isinstance(result, (tuple, list)):
+        sub_annotation = []
+        for j, r in enumerate(result):
+          sub_annotation.append(Result(r.placement, f"output{i}_{j}"))
+        updated_annotation.append(sub_annotation)
+    edsl_sig = edsl_sig.replace(return_annotation=updated_annotation)
+  elif output_placements is not None:
+    pass
+  else:
+    raise ValueError
 
+  @edsl.computation
+  @functools.wraps(edsl_func)
+  def computation_with_storage(*args):  # TODO give these args proper edsl.Argument type hints (if it matters)
+    input_values = []
+    breakpoint()
+    for i, arg in enumerate(args):
+      arg_dtype = parameters[i].dtype
+      arg_placement = parameters[i].placement
+      input_values.append(edsl.load(arg, dtype=arg_dtype, placement=arg_placement))
+    return_values = edsl_func(*input_values)
+    output_ops = []
+    for a, v in zip(edsl_sig.return_annotation, return_values):
+      if isinstance(a, edsl.Result):
+        op = edsl.save(key=a.name, value=v, placement=a.placement)
+      elif isinstance(a, (tuple, list)):
+        op = [edsl.save(ai.name, v, placement=ai.placement) for ai in a]
+      output_ops.append(op)
 
-def place_outputs(output_placements):
-  def wrapper(abstract_computation):
-    edsl_func = abstract_computation.func
+    return output_ops
 
+  return computation_with_storage
 
+@place_computation
+@edsl.computation
+def other(
+  x: edsl.Argument(alice, dtype=edsl.float32),
+  y: edsl.Argument(bob, dtype=edsl.float32),
+) -> tuple(
+    (
+      edsl.Result(bob, name="bobs_x_result"),
+      edsl.Result(alice),
+      (edsl.Result(alice), edsl.Result(bob)),
+    )
+):
+  with multiply_placement:
+    z = edsl.mul(x, y)
+  return x, y, z
 
 
 ##### EXAMPLE TASK EXPECTED FROM PYCAPE DEV AND/OR USER ######
@@ -178,8 +238,6 @@ def build_multiplication(secret, x_placement_name, y_placement_name):
 
   return other, simple_multiplication
 
-breakpoint()
-
 ##############################################################
 
 
@@ -199,142 +257,138 @@ breakpoint()
 # flags)
 # right now it's pretty tied into the Moose compiler interface, which is likely to
 # change in the near future
-# class CompilerConfig:
-#   def __init__(self, compiler_passes=None, render=False):
-#     self.passes = compiler_passes
-#     self.render = render
+class CompilerConfig:
+  def __init__(self, compiler_passes=None, render=False):
+    self.passes = compiler_passes
+    self.render = render
 
 
-# class GraphQLMixin(metaclass=abc.ABCMeta):
-#   @abc.abstractmethod
-#   def lift_to_graphql_request(self, **kwargs):  # TODO: function sig?
-#     pass
+class GraphQLMixin(metaclass=abc.ABCMeta):
+  @abc.abstractmethod
+  def lift_to_graphql_request(self, **kwargs):  # TODO: function sig?
+    pass
 
-#   def submit_job(self, **kwargs):  # TODO: function sig?
-#     self.lift_to_graphql_request(**kwargs)
+  def submit_job(self, **kwargs):  # TODO: function sig?
+    self.lift_to_graphql_request(**kwargs)
 
 
-# class MooseMixin:
-#   def __init__(self, comp, compiler_config=None):
-#     self._logical_comp = comp
-#     self._compiler_config = compiler_config or CompilerConfig()
-#     self._compiler = Compiler()
-#     self._networking = None
-#     self._storage_dict = None
-#     self._executor_dict = None
-#     self._local_runtime = None
+class MooseMixin:
+  def __init__(self, comp, compiler_config=None):
+    self._logical_comp = comp
+    self._compiler_config = compiler_config or CompilerConfig()
+    self._compiler = Compiler()
+    self._networking = None
+    self._storage_dict = None
+    self._executor_dict = None
+    self._local_runtime = None
 
-#   @classmethod
-#   def from_computation_object(cls, comp):
-#     if not isinstance(comp, Computation):
-#       raise ValueError(f"wat? {type(comp)}, not: Computation")
-#     return cls(comp)
+  @classmethod
+  def from_computation_object(cls, comp):
+    if not isinstance(comp, Computation):
+      raise ValueError(f"wat? {type(comp)}, not: Computation")
+    return cls(comp)
 
-#   def _find_host_placements(self, comp):
-#     host_placements = dict()
-#     for name, placement in comp.placements.items():
-#       if isinstance(placement, HostPlacement):
-#         host_placements[name] = placement
-#       elif hasattr(placement, "player_names"):
-#         # replicated or mpspdz placement
-#         for name in placement.player_names:
-#           player_placement = comp.placement(name)
-#           assert isinstance(player_placement, HostPlacement)
-#           host_placements.update({name: player_placement})
-#     return host_placements
+  def _find_host_placements(self, comp):
+    host_placements = dict()
+    for name, placement in comp.placements.items():
+      if isinstance(placement, HostPlacement):
+        host_placements[name] = placement
+      elif hasattr(placement, "player_names"):
+        # replicated or mpspdz placement
+        for name in placement.player_names:
+          player_placement = comp.placement(name)
+          assert isinstance(player_placement, HostPlacement)
+          host_placements.update({name: player_placement})
+    return host_placements
 
-#   @property
-#   def placements(self):
-#     if self._placements is None:
-#       self._placements = self._find_host_placements(self._logical_comp)
-#     return self._placements
+  @property
+  def placements(self):
+    if self._placements is None:
+      self._placements = self._find_host_placements(self._logical_comp)
+    return self._placements
 
-#   def seed_storage(self, placement, *, **keyvalue_store):
-#     # may need to provide initial storage values for some placements
-#     assert self._local_runtime is not None, "Must build local runtime."
+  def seed_storage(self, placement, *, **keyvalue_store):
+    # may need to provide initial storage values for some placements
+    assert self._local_runtime is not None, "Must build local runtime."
 
-#   def _build_local_runtime(self, force_rebuild=False):
-#     if self._local_runtime is None or force_rebuild:
-#       networking = Networking()
-#       # Storage for each executor
-#       storage_dict = {plc.name: MemoryDataStore() for plc in self.placements}
-#       # Executors
-#       executor_dict = {
-#         plc.name: AsyncExecutor(networking, storage=storage_dict[plc.name])
-#         for plc in self.placements
-#       }
-#       # Runtime
-#       self._local_runtime = TestRuntime(networking=networking, backing_executors=executor_dict)
+  def _build_local_runtime(self, force_rebuild=False):
+    if self._local_runtime is None or force_rebuild:
+      networking = Networking()
+      # Storage for each executor
+      storage_dict = {plc.name: MemoryDataStore() for plc in self.placements}
+      # Executors
+      executor_dict = {
+        plc.name: AsyncExecutor(networking, storage=storage_dict[plc.name])
+        for plc in self.placements
+      }
+      # Runtime
+      self._local_runtime = TestRuntime(networking=networking, backing_executors=executor_dict)
     
-#   def _pretty_print_results(self):
-#     # need to iterate through executors and pretty print their MemoryDataStore contents
-#     # if wanted to get fancy could only print stuff that wasn't seeded before evaluation
-#     # i.e., do something with:
-#     # {plc: ex.storage.store for plc, ex in self._local_runtime.existing_executors.items()}
+  def _pretty_print_results(self):
+    # need to iterate through executors and pretty print their MemoryDataStore contents
+    # if wanted to get fancy could only print stuff that wasn't seeded before evaluation
+    # i.e., do something with:
+    # {plc: ex.storage.store for plc, ex in self._local_runtime.existing_executors.items()}
 
-#     # alternatively, moose could support directly reporting outputs to this python process
-#     pass
+    # alternatively, moose could support directly reporting outputs to this python process
+    pass
 
-#   def local_dry_run(self, *, verbose=False, **computation_kwargs):
-#     compiler_flags = {
-#       "passes": None,
-#       "render": verbose,
-#     }
-#     computation = self._compiler.compile(self._logical_comp, **compiler_flags)
-#     self._build_local_runtime(False)
-#     plc_instantiation = {plc: plc.name for plc in self.placements}
-#     # evaluate computation
-#     self._local_runtime.evaluate_computation(
-#       computation,
-#       placement_instantiation=plc_instantiation,
-#       arguments=computation_kwargs,
-#     )
-#     # right now there is no clean way to return results, since we don't know which
-#     # placements should contain saved values.
+  def local_dry_run(self, *, verbose=False, **computation_kwargs):
+    compiler_flags = {
+      "passes": None,
+      "render": verbose,
+    }
+    computation = self._compiler.compile(self._logical_comp, **compiler_flags)
+    self._build_local_runtime(False)
+    plc_instantiation = {plc: plc.name for plc in self.placements}
+    # evaluate computation
+    self._local_runtime.evaluate_computation(
+      computation,
+      placement_instantiation=plc_instantiation,
+      arguments=computation_kwargs,
+    )
+    # right now there is no clean way to return results, since we don't know which
+    # placements should contain saved values.
 
-#     self._pretty_print_results()
-#     return self  # yes/no??
-
-
-# class MooseEdslMixin(MooseMixin):
-#   @classmethod
-#   def from_edsl_func(cls, func):
-#     if not isinstance(func, AbstractComputation):
-#       raise ValueError(f"wat? {type(func)}, not: AbstractComputation")
-#     # it sucks that we need to expose compiler config here; 
-#     # we should be tracing here and compiling later
-#     logical_comp = edsl.trace(func)
-#     return cls.from_computation_object(logical_comp)
+    self._pretty_print_results()
+    return self  # yes/no??
 
 
-# class MultiplicationTask(MooseEdslMixin, GraphQLMixin):
-#   def __init__(self, x_input: DataView, y_input: DataView, secret=False):
-#     x_placement_name = super(GraphQLMixin).get_placement_name_from_organization(x_input.owner)
-#     y_placement_name = super(GraphQLMixin).get_placement_name_from_organization(y_input.owner)
-#     mult_edsl_func = build_multiplication(
-#       secret=secret,
-#       x_placement_name=x_placement_name,
-#       y_placement_name=y_placement_name,
-#     )
-#     super().from_edsl_func(mult_edsl_func)
+class MooseEdslMixin(MooseMixin):
+  @classmethod
+  def from_edsl_func(cls, func):
+    if not isinstance(func, AbstractComputation):
+      raise ValueError(f"wat? {type(func)}, not: AbstractComputation")
+    # it sucks that we need to expose compiler config here; 
+    # we should be tracing here and compiling later
+    logical_comp = edsl.trace(func)
+    return cls.from_computation_object(logical_comp)
 
 
-# mul = MultiplicationTask("alice", "bob", secret=False)  # x * y = z
-# z = mul.run(x=np.array([1.0]), y=np.array([2.0]))
-# print(z)  # np.array([3.0])
+class MultiplicationTask(MooseEdslMixin, GraphQLMixin):
+  def __init__(self, x_input: DataView, y_input: DataView, secret=False):
+    x_placement_name = super(GraphQLMixin).get_placement_name_from_organization(x_input.owner)
+    y_placement_name = super(GraphQLMixin).get_placement_name_from_organization(y_input.owner)
+    mult_edsl_func = build_multiplication(
+      secret=secret,
+      x_placement_name=x_placement_name,
+      y_placement_name=y_placement_name,
+    )
+    super().from_edsl_func(mult_edsl_func)
 
 
-# mul = MultiplicationTask("alice", "bob", secret=False)  # x * y = z
-# mul.seed_storage("alice", {"x": np.array([1.0])})
-# mul.seed_storage("bob", {"y": np.array([2.0])})
-# mul.run(x_key="x", y_key="y", out_key="result")
-# print(mul.get_results())
-# # result: np.array([3.0])
+mul = MultiplicationTask("alice", "bob", secret=False)  # x * y = z
+z = mul.run(x=np.array([1.0]), y=np.array([2.0]))
+print(z)  # np.array([3.0])
 
+mul = MultiplicationTask("alice", "bob", secret=False)  # x * y = z
+mul.seed_storage("alice", {"x": np.array([1.0])})
+mul.seed_storage("bob", {"y": np.array([2.0])})
+mul.run(x_key="x", y_key="y", out_key="result")
+print(mul.get_results())
+# result: np.array([3.0])
 
-# print(z)  # np.array([3.0])
-
-# # WAY IN THE FUTURE STUFF
-# class JaxTask(MooseMixin):
-#   @classmethod
-#   def from_jax_comp(cls, comp): pass  # use jax tracer to produce a fully-functional MooseTask
+# WAY IN THE FUTURE STUFF
+class JaxTask(MooseMixin):
+  @classmethod
+  def from_jax_comp(cls, comp): pass  # use jax tracer to produce a fully-functional MooseTask
