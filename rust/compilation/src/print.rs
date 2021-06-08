6d155c1d2ac05000fe78a3cadb03034425ac1b26
Lex Vorona <lex@capeprivacy.com>
2021-06-08 14:56:30 -0700

    Use custom attributes for pretty graph visualization

--- rust/compilation/src/print.rs
+++ rust/compilation/src/print.rs
@@ -1,17 +1,61 @@
 use moose::computation::{Computation, Operation, Operator, ReceiveOp};
 use moose::text_computation::ToTextual;
+use petgraph::dot::Config::{EdgeNoLabel, NodeNoLabel};
 use petgraph::dot::Dot;
-use petgraph::graph::{EdgeIndex, Graph};
+use petgraph::visit::EdgeRef;
+use std::collections::HashMap;
+
+const COLORS: [&str; 9] = [
+    "#336699", "#ff0000", "#ff6600", "#92cd00", "#ffcc00", "#ffa4b6", "#f765a3", "#a155b9",
+    "#3caea3",
+];
 
 /// Prints the computation's graph DOT representation to stdout
 pub fn print_graph(comp: &Computation) -> anyhow::Result<Computation> {
     let graph = comp.as_graph();
-    let graph2 = graph.map(
-        |_, n| pretty(&comp.operations[n.1]),
-        |e, _| pretty_edge(&graph, comp, e),
+
+    // We need to compute the color lookup table ahead of time, because `Dot`'s closures capture everything as immutable
+    let mut color_cache = HashMap::new();
+    let mut color = 0..;
+    for n in graph.node_indices() {
+        let placement = comp.operations[graph[n].1].placement.to_textual();
+        color_cache
+            .entry(placement)
+            .or_insert_with(|| COLORS[color.next().unwrap_or_default() % COLORS.len()]);
+    }
+
+    println!(
+        "{:?}",
+        Dot::with_attr_getters(
+            &graph,
+            // We will be manually adding label elements, turn off automatic labels.
+            &[EdgeNoLabel, NodeNoLabel],
+            // Edge formatter.
+            &|_, edge| {
+                let source = &comp.operations[graph[edge.source()].1];
+                let target = &comp.operations[graph[edge.target()].1];
+                // Annotate jumps in between the hosts
+                match (&source.kind, &target.kind) {
+                    (
+                        Operator::Send(_),
+                        Operator::Receive(ReceiveOp {
+                            rendezvous_key: key,
+                            ..
+                        }),
+                    ) => format!("label={} style = dotted", key),
+                    _ => "".into(),
+                }
+            },
+            // Node formatter.
+            &|_, (_, n)| {
+                format!(
+                    "label = \"{}\" shape = rectangle color = \"{}\"",
+                    pretty(&comp.operations[n.1]),
+                    color_cache[&comp.operations[n.1].placement.to_textual()]
+                )
+            }
+        )
     );
-    // Place Config::EdgeNoLabel inside the config element to have no labels on edges
-    println!("{}", Dot::with_config(&graph2, &[]));
     Ok(Computation {
         operations: comp.operations.clone(),
     })
@@ -66,21 +110,5 @@ fn pretty(op: &Operation) -> String {
         Operator::FixedpointRingDecode(_) => "FixedpointRingDecode",
         Operator::FixedpointRingMean(_) => "FixedpointRingMean",
     };
-    format!("{} = {}\n{}", op.name, op_kind, op.placement.to_textual())
-}
-
-fn pretty_edge(g: &Graph<(String, usize), ()>, comp: &Computation, e: EdgeIndex) -> String {
-    let edge = &g.raw_edges()[e.index()];
-    let source = &comp.operations[g[edge.source()].1];
-    let target = &comp.operations[g[edge.target()].1];
-    match (&source.kind, &target.kind) {
-        (
-            Operator::Send(_),
-            Operator::Receive(ReceiveOp {
-                rendezvous_key: key,
-                ..
-            }),
-        ) => key.into(),
-        _ => "".into(),
-    }
+    format!("{} = {}\\l{}", op.name, op_kind, op.placement.to_textual())
 }
