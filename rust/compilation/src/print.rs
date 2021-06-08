bab58f3e46c2da0df1c315acc26bdfa1c67a6708
Lex Vorona <lex@capeprivacy.com>
2021-06-08 11:43:56 -0700

    Make the printed graph a bit more usable

--- rust/compilation/src/print.rs
+++ rust/compilation/src/print.rs
@@ -1,11 +1,64 @@
-use moose::computation::Computation;
+use moose::computation::{Computation, Operation, Operator};
 use petgraph::dot::Dot;
 
 /// Prints the computation's graph DOT representation to stdout
 pub fn print_graph(comp: &Computation) -> anyhow::Result<Computation> {
     let graph = comp.as_graph();
-    println!("{:?}", Dot::new(&graph));
+    let graph2 = graph.map(|_, n| pretty(&comp.operations[n.1]), |_, e| e);
+    println!("{:?}", Dot::new(&graph2));
     Ok(Computation {
         operations: comp.operations.clone(),
     })
+}
+
+/// Prints only the bits of the operation that we want visible on the graph's DOT
+fn pretty(op: &Operation) -> String {
+    let op_kind = match op.kind {
+        Operator::Identity(_) => "Identity",
+        Operator::Load(_) => "Load",
+        Operator::Save(_) => "Save",
+        Operator::Send(_) => "Send",
+        Operator::Receive(_) => "Receive",
+        Operator::Input(_) => "Input",
+        Operator::Output(_) => "Output",
+        Operator::Constant(_) => "Constant",
+        Operator::StdAdd(_) => "StdAdd",
+        Operator::StdSub(_) => "StdSub",
+        Operator::StdMul(_) => "StdMul",
+        Operator::StdDiv(_) => "StdDiv",
+        Operator::StdDot(_) => "StdDot",
+        Operator::StdMean(_) => "StdMean",
+        Operator::StdExpandDims(_) => "StdExpandDims",
+        Operator::StdReshape(_) => "StdReshape",
+        Operator::StdAtLeast2D(_) => "StdAtLeast2D",
+        Operator::StdShape(_) => "StdShape",
+        Operator::StdSlice(_) => "StdSlice",
+        Operator::StdSum(_) => "StdSum",
+        Operator::StdOnes(_) => "StdOnes",
+        Operator::StdConcatenate(_) => "StdConcatenate",
+        Operator::StdTranspose(_) => "StdTranspose",
+        Operator::StdInverse(_) => "StdInverse",
+        Operator::RingAdd(_) => "RingAdd",
+        Operator::RingSub(_) => "RingSub",
+        Operator::RingMul(_) => "RingMul",
+        Operator::RingDot(_) => "RingDot",
+        Operator::RingSum(_) => "RingSum",
+        Operator::RingShape(_) => "RingShape",
+        Operator::RingSample(_) => "RingSample",
+        Operator::RingFill(_) => "RingFill",
+        Operator::RingShl(_) => "RingShl",
+        Operator::RingShr(_) => "RingShr",
+        Operator::RingInject(_) => "RingInject",
+        Operator::BitExtract(_) => "BitExtract",
+        Operator::BitSample(_) => "BitSample",
+        Operator::BitFill(_) => "BitFill",
+        Operator::BitXor(_) => "BitXor",
+        Operator::BitAnd(_) => "BitAnd",
+        Operator::PrimDeriveSeed(_) => "PrimDeriveSeed",
+        Operator::PrimGenPrfKey(_) => "PrimGenPrfKey",
+        Operator::FixedpointRingEncode(_) => "FixedpointRingEncode",
+        Operator::FixedpointRingDecode(_) => "FixedpointRingDecode",
+        Operator::FixedpointRingMean(_) => "FixedpointRingMean",
+    };
+    format!("{}, {}", op.name, op_kind)
 }
