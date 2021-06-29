ee382cbb078ea4277091c41da7dca6b96fba16ad
jvmancuso <jason@manc.us>
2021-06-29 17:18:01 -0400

    adding python tests for new capabilities, updating test form

--- rust/pymoose/moose_kernels_test.py
+++ rust/pymoose/moose_kernels_test.py
@@ -1,4 +1,3 @@
-from moose.computation.standard import StringType
 import random
 
 import numpy as np
@@ -8,6 +7,7 @@ from pymoose import MooseLocalRuntime
 from pymoose import moose_kernels as mkls
 
 from moose import edsl
+from moose.computation.standard import StringType
 from moose.computation.utils import serialize_computation
 
 
@@ -137,68 +137,130 @@ x_owner = edsl.host_placement(name="x_owner")
 y_owner = edsl.host_placement(name="y_owner")
 output_owner = edsl.host_placement("output_owner")
 
-x_input = {"x": np.array([1.0], dtype=np.float64)}
-y_input = {"y": np.array([2.0], dtype=np.float64)}
-
-add_storage = {
-    "x_owner": x_input,
-    "y_owner": y_input,
-    "output_owner": {},
-}
 
 @edsl.computation
-def add_comp(
+def add_full_storage(
     x_key: edsl.Argument(x_owner, vtype=StringType()),
-    y_key: edsl.Argument(y_owner, vtype=StringType())
+    y_key: edsl.Argument(y_owner, vtype=StringType()),
 ):
     with x_owner:
         x = edsl.load(x_key, dtype=edsl.float64)
-
     with y_owner:
         y = edsl.load(y_key, dtype=edsl.float64)
-
     with output_owner:
         out = edsl.add(x, y)
         res = edsl.save("output", out)
+    return res
 
-    return [res , out]
-
-
-add_args_storage = {
-    "x_owner": {},
-    "y_owner": {},
-    "output_owner": {},
-}
-add_args_input = {**x_input, **y_input}
 
 @edsl.computation
-def add_args_comp(
+def add_input_storage(
+    x_key: edsl.Argument(x_owner, vtype=StringType()),
+    y_key: edsl.Argument(y_owner, vtype=StringType()),
+):
+    with x_owner:
+        x = edsl.load(x_key, dtype=edsl.float64)
+    with y_owner:
+        y = edsl.load(y_key, dtype=edsl.float64)
+    with output_owner:
+        out = edsl.add(x, y)
+    return out
+
+
+@edsl.computation
+def add_output_storage(
     x: edsl.Argument(x_owner, dtype=edsl.float64),
     y: edsl.Argument(y_owner, dtype=edsl.float64),
 ):
     with output_owner:
         out = edsl.add(x, y)
-        res = edsl.save("output", out)
-
+        res = edsl.save(out, "output")
     return res
 
 
+@edsl.computation
+def add_no_storage(
+    x: edsl.Argument(x_owner, dtype=edsl.float64),
+    y: edsl.Argument(y_owner, dtype=edsl.float64),
+):
+    with output_owner:
+        out = edsl.add(x, y)
+    return out
+
+
+@edsl.computation
+def add_multioutput(
+    x: edsl.Argument(x_owner, dtype=edsl.float64),
+    y: edsl.Argument(y_owner, dtype=edsl.float64),
+):
+    with output_owner:
+        out = edsl.add(x, y)
+    return out, x, y
+
+
 class RunComputation(parameterized.TestCase):
-    @parameterized.parameters(
-        (add_comp, add_storage, {"x_key": "x", "y_key": "y"}),
-        # (add_args_comp, add_args_storage, add_args_input),
-    )
-    def test_run_computation(self, comp, storage_dict, args):
+    def setUp(self):
+        self.x_input = {"x": np.array([1.0], dtype=np.float64)}
+        self.y_input = {"y": np.array([2.0], dtype=np.float64)}
+        self.storage_dict = {
+            "x_owner": self.x_input,
+            "y_owner": self.y_input,
+            "output_owner": {},
+        }
+        self.empty_storage = {
+            "x_owner": {},
+            "y_owner": {},
+            "output_owner": {},
+        }
+        self.storage_args = {"x_key": "x", "y_key": y}
+        self.actual_args = {**self.x_input, **self.y_input}
+
+    def _inner_prepare_runtime(self, comp, storage_dict):
         concrete_comp = edsl.trace_and_compile(comp, ring=128)
         comp_bin = serialize_computation(concrete_comp)
-        print(concrete_comp)
-
         runtime = MooseLocalRuntime(executors_storage=storage_dict)
-        outputs = runtime.evaluate_computation(comp_bin, args)
+        return comp_bin, runtime
+
+    def test_full_storage(self):
+        comp_bin, runtime = self._inner_prepare_runtime(
+            add_full_storage, self.storage_dict
+        )
+        outputs = runtime.evaluate_computation(comp_bin, self.storage_args)
+        assert len(outputs) == 0
         result = runtime.get_value_from_storage("output_owner", "output")
         np.testing.assert_array_equal(result, np.array([3.0]))
-        # [Yann] why is the shape (1, 1)?
-        np.testing.assert_array_equal(outputs, np.array([[3.0]]))
+
+    def test_input_storage(self):
+        comp_bin, runtime = self._inner_prepare_runtime(
+            add_input_storage, self.storage_dict
+        )
+        result = runtime.evaluate_computation(comp_bin, self.storage_args)
+        np.testing.assert_array_equal(result, np.array([3.0]))
+
+    def test_output_storage(self):
+        comp_bin, runtime = self._inner_prepare_runtime(
+            add_output_storage, self.empty_storage
+        )
+        outputs = runtime.evaluate_computation(comp_bin, self.actual_args)
+        assert len(outputs) == 0
+        result = runtime.get_value_from_storage("output_owner", "output")
+        np.testing.assert_array_equal(result, np.array([3.0]))
+
+    def test_no_storage(self):
+        comp_bin, runtime = self._inner_prepare_runtime(
+            add_no_storage, self.storage_dict
+        )
+        result = runtime.evaluate_computation(comp_bin, self.actual_args)
+        np.testing.assert_array_equal(result, np.array([3.0]))
+
+    def test_multioutput(self):
+        comp_bin, runtime = self._inner_prepare_runtime(
+            add_no_storage, self.storage_dict
+        )
+        (out, x, y) = runtime.evaluate_computation(comp_bin, self.actual_args)
+        np.testing.assert_array_equal(x, np.array([1.0]))
+        np.testing.assert_array_equal(y, np.array([2.0]))
+        np.testing.assert_array_equal(out, np.array([3.0]))
 
 
 if __name__ == "__main__":
