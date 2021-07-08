a10b4f1d8019b9b7998851ae4d9c960163650310
Dragos Rotaru <r.dragos0@gmail.com>
2021-07-08 17:51:23 +0300

    macro test

--- rust/moose/src/replicated.rs
+++ rust/moose/src/replicated.rs
@@ -1078,37 +1078,110 @@ mod tests {
         test_rep_add128(x, y, z);
     }
 
-    #[test]
-    fn test_rep_truncation() {
-        let alice = HostPlacement {
-            owner: "alice".into(),
+    macro_rules! rep_truncation_test {
+        ($func_name:ident, $tt: ident) => {
+            fn $func_name(xs: ArrayD<$tt>, amount: usize, ys: ArrayD<$tt>) {
+                let alice = HostPlacement {
+                    owner: "alice".into(),
+                };
+                let bob = HostPlacement {
+                    owner: "bob".into(),
+                };
+                let carole = HostPlacement {
+                    owner: "carole".into(),
+                };
+
+                let rep = ReplicatedPlacement {
+                    owners: ["alice".into(), "bob".into(), "carole".into()],
+                };
+
+                let sess = SyncSession::default();
+                let setup = rep.gen_setup(&sess);
+
+                let alice_x1 = AbstractRingTensor::from_raw_plc(xs.clone(), alice.clone());
+                let alice_rep = rep.share(&sess, &setup, &alice_x1);
+                let alice_tr = rep.trunc_pr(&sess, amount, &alice_rep);
+                let alice_open = alice.reveal(&sess, &alice_tr);
+
+                let alice_y = AbstractRingTensor::from_raw_plc(ys.clone(), alice.clone());
+                assert_eq!(alice_open.1, alice_y.1); // make sure placements are equal
+
+                // truncation can be off by 1
+                let alice_diff = alice_y - alice_open;
+                println!("{:?}", alice_diff);
+                assert!(
+                    (alice_diff.0.iter().max().unwrap().clone() -
+                    alice_diff.0.iter().min().unwrap().clone()) <= std::num::Wrapping(1)
+                );
+
+
+                // let bob_x1 = AbstractRingTensor::from_raw_plc(xs.clone(), bob.clone());
+                // let bob_rep = rep.share(&sess, &setup, &bob_x1);
+                // let bob_tr = rep.trunc_pr(&sess, amount, &bob_rep);
+                // let bob_open = bob.reveal(&sess, &bob_tr);
+
+                // let bob_y = AbstractRingTensor::from_raw_plc(ys.clone(), bob.clone());
+                // assert_eq!(bob_open.1, bob);
+
+                // let bob_diff = bob_y - bob_open;
+                // assert!(
+                //     (bob_diff.0.iter().max().unwrap().clone() -
+                //     bob_diff.0.iter().min().unwrap().clone()) <= std::num::Wrapping(1)
+                // );
+
+
+                // let carole_x1 = AbstractRingTensor::from_raw_plc(xs.clone(), carole.clone());
+                // let carole_rep = rep.share(&sess, &setup, &carole_x1);
+                // let carole_tr = rep.trunc_pr(&sess, amount, &carole_rep);
+                // let carole_open = carole.reveal(&sess, &carole_tr);
+
+                // let carole_y = AbstractRingTensor::from_raw_plc(ys.clone(), bob.clone());
+                // assert_eq!(carole_open.1, carole);
+
+                // let carole_diff = carole_y - carole_open;
+                // assert!(
+                //     (carole_diff.0.iter().max().unwrap().clone() -
+                //     carole_diff.0.iter().min().unwrap().clone()) <= std::num::Wrapping(1)
+                // );
+
+            }
         };
-        let bob = HostPlacement {
-            owner: "bob".into(),
-        };
-        let carole = HostPlacement {
-            owner: "carole".into(),
-        };
+    }
 
-        let rep = ReplicatedPlacement {
-            owners: ["alice".into(), "bob".into(), "carole".into()],
-        };
+    rep_truncation_test!(test_rep_truncation64, u64);
+    rep_truncation_test!(test_rep_truncation128, u128);
 
-        let sess = SyncSession::default();
-        let setup = rep.gen_setup(&sess);
-        let x1 = AbstractRingTensor::from_raw_plc(array![80908_u64, 0, 40454], alice.clone());
-        let x1_rep = rep.share(&sess, &setup, &x1);
+    #[rstest]
+    #[case(array![1_u64, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328, 281474976710656, 562949953421312, 1125899906842624, 2251799813685248, 4503599627370496, 9007199254740992, 18014398509481984, 36028797018963968, 72057594037927936, 144115188075855872, 288230376151711744, 576460752303423488, 1152921504606846976, 2305843009213693952, 4611686018427387904].into_dyn(),
+        1,
+        array![0_u64, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328, 281474976710656, 562949953421312, 1125899906842624, 2251799813685248, 4503599627370496, 9007199254740992, 18014398509481984, 36028797018963968, 72057594037927936, 144115188075855872, 288230376151711744, 576460752303423488, 1152921504606846976, 2305843009213693952].into_dyn())
+    ]
+    #[case(array![1_u64, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328, 281474976710656, 562949953421312, 1125899906842624, 2251799813685248, 4503599627370496, 9007199254740992, 18014398509481984, 36028797018963968, 72057594037927936, 144115188075855872, 288230376151711744, 576460752303423488, 1152921504606846976, 2305843009213693952, 4611686018427387904].into_dyn(),
+        62,
+        array![0_u64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1].into_dyn())
+    ]
+    #[case(array![1_u64, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328, 281474976710656, 562949953421312, 1125899906842624, 2251799813685248, 4503599627370496, 9007199254740992, 18014398509481984, 36028797018963968, 72057594037927936, 144115188075855872, 288230376151711744, 576460752303423488, 1152921504606846976, 2305843009213693952, 4611686018427387904].into_dyn(),
+        61,
+        array![0_u64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2].into_dyn())
+    ]
+    fn test_rep_truncation_64(
+        #[case] x: ArrayD<u64>,
+        #[case] amount: usize,
+        #[case] target: ArrayD<u64>,
+    ) {
+        test_rep_truncation64(x, amount, target);
+    }
 
-        let truncated = rep.trunc_pr(&sess, 1, &x1_rep);
-        let x1_open = alice.reveal(&sess, &truncated);
-
-        let y1 = AbstractRingTensor::from_raw_plc(array![40454, 0, 20227], alice);
-        assert_eq!(x1_open.1, y1.1); // make sure placements are equal
-
-        let z = y1 - x1_open;
-        let min_diff = z.0.iter().max().unwrap().clone();
-        let max_diff = z.0.iter().min().unwrap().clone();
-
-        assert!((max_diff - min_diff) <= std::num::Wrapping(1)); // truncation can be off by 1
+    #[rstest]
+    #[case(array![80908_u128, 0, 40454].into_dyn(),
+        1,
+        array![40454_u128, 0, 20227].into_dyn())
+    ]
+    fn test_rep_truncation_128(
+        #[case] x: ArrayD<u128>,
+        #[case] amount: usize,
+        #[case] target: ArrayD<u128>,
+    ) {
+        test_rep_truncation128(x, amount, target);
     }
 }
