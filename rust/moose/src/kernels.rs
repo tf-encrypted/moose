213b7e7bdb5efa5922a9ba28446aaa0029800bfd
Lex Vorona <lex@capeprivacy.com>
2021-07-08 10:35:53 -0700

    Python bindings for Rust compiler (#389)

--- rust/moose/src/kernels.rs
+++ rust/moose/src/kernels.rs
@@ -1150,7 +1150,9 @@ impl Compile<SyncKernel> for ReceiveOp {
             let v: Value = sess
                 .networking
                 .receive(&sender_id, &rendezvous_key, &sess.sid)?;
-            check_type(&v, expected_ty)?;
+            if expected_ty != Ty::Unknown {
+                check_type(&v, expected_ty)?;
+            }
             Ok(v)
         })))
     }
@@ -1179,7 +1181,9 @@ impl Compile<AsyncKernel> for ReceiveOp {
                     .networking
                     .receive(&sender_id, &rendezvous_key, &sess.sid)
                     .await?;
-                check_type(&v, expected_ty)?;
+                if expected_ty != Ty::Unknown {
+                    check_type(&v, expected_ty)?;
+                }
                 map_send_result(sender.send(v))
             })
         })))
