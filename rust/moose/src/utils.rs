c3cf9c93c915214cf203eccbc729b1b5bcd2ece9
Justin Patriquin <justinpatriquin@gmail.com>
2021-08-04 11:58:17 -0300

    get version test

--- rust/moose/src/utils.rs
+++ rust/moose/src/utils.rs
@@ -13,6 +13,10 @@ pub fn derive_seed(key: &[u8], nonce: &[u8]) -> RngSeed {
     output
 }
 
+pub fn get_version() -> &'static str {
+    env!("CARGO_PKG_VERSION")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
