1e492c9a516d9dc3537db2add7141f8517f64911
Morten Dahl <mortendahlcs@gmail.com>
2021-09-29 16:30:34 +0200

    ..

--- moose/src/circuits/bristol_fashion.rs
+++ moose/src/circuits/bristol_fashion.rs
@@ -3,17 +3,94 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{newline, space0, u64};
-use nom::combinator::value;
-use nom::error::Error;
+use nom::combinator::{all_consuming, value};
 use nom::multi::{length_count, many0, many_m_n, separated_list0};
 use nom::sequence::{delimited, terminated, tuple};
-use std::io::{self, prelude::*, BufReader};
+use std::convert::TryFrom;
+
+use crate::kernels::{PlacementAnd, PlacementNeg, PlacementXor, Session};
+
+const AES_128: &[u8] = include_bytes!("aes_128.txt");
+
+pub fn aes<S: Session, P, BitT>(sess: &S, plc: &P, k: Vec<BitT>, m: Vec<BitT>) -> Vec<BitT>
+where
+    BitT: Clone,
+    P: PlacementXor<S, BitT, BitT, BitT>,
+    P: PlacementAnd<S, BitT, BitT, BitT>,
+    P: PlacementNeg<S, BitT, BitT>,
+{
+    let circuit = Circuit::try_from(AES_128).unwrap();
+    let mut wires: Vec<Option<BitT>> = vec![None; circuit.num_wires];
+
+    assert_eq!(k.len(), 128);
+    for (i, val) in k.into_iter().enumerate() {
+        *wires.get_mut(i).unwrap() = Some(val);
+    }
+
+    assert_eq!(m.len(), 128);
+    for (i, val) in m.into_iter().enumerate() {
+        *wires.get_mut(i + 128).unwrap() = Some(val);
+    }
+
+    for gate in circuit.gates {
+        use GateKind::*;
+        match gate.kind {
+            Xor => {
+                let x_wire = *gate.input_wires.get(0).unwrap();
+                let y_wire = *gate.input_wires.get(1).unwrap();
+                let x = wires.get(x_wire).unwrap().clone().unwrap();
+                let y = wires.get(y_wire).unwrap().clone().unwrap();
+
+                let z = plc.xor(sess, &x, &y);
+                let z_wire = *gate.output_wires.get(0).unwrap();
+                *wires.get_mut(z_wire).unwrap() = Some(z);
+            }
+            And => {
+                let x_wire = *gate.input_wires.get(0).unwrap();
+                let y_wire = *gate.input_wires.get(1).unwrap();
+                let x = wires.get(x_wire).unwrap().clone().unwrap();
+                let y = wires.get(y_wire).unwrap().clone().unwrap();
+
+                let z = plc.and(sess, &x, &y);
+                let z_wire = *gate.output_wires.get(0).unwrap();
+                *wires.get_mut(z_wire).unwrap() = Some(z);
+            }
+            Inv => {
+                let x_wire = *gate.input_wires.get(0).unwrap();
+
+                let x = wires.get(x_wire).unwrap().clone().unwrap();
+
+                let y = plc.neg(sess, &x);
+                let y_wire = *gate.output_wires.get(0).unwrap();
+                *wires.get_mut(y_wire).unwrap() = Some(y);
+            }
+        }
+    }
+
+    wires.into_iter().rev().take(128).map(|val| val.unwrap()).collect() // TODO
+}
 
 #[derive(Debug)]
 pub struct Circuit {
-    number_of_gates: usize,
-    number_of_wires: usize,
+    num_gates: usize,
+    num_wires: usize,
+    num_inputs: usize,
+    input_wires: Vec<usize>,
+    num_outputs: usize,
+    output_wires: Vec<usize>,
     gates: Vec<Gate>,
+}
+
+impl TryFrom<&[u8]> for Circuit {
+    type Error = crate::error::Error;
+    fn try_from(bytes: &[u8]) -> Result<Circuit, Self::Error> {
+        parse_circuit(bytes)
+            .map_err(|e| {
+                println!("{:?}", e);
+                crate::error::Error::Unexpected
+            })
+            .map(|res| res.1)
+    }
 }
 
 #[derive(Debug)]
@@ -30,7 +107,7 @@ pub enum GateKind {
     Inv,
 }
 
-type Res<T, U> = nom::IResult<T, U, Error<T>>;
+type Res<T, U> = nom::IResult<T, U, nom::error::Error<T>>;
 
 fn parse_circuit(bytes: &[u8]) -> Res<&[u8], Circuit> {
     // First line is just two usize values
@@ -43,20 +120,21 @@ fn parse_circuit(bytes: &[u8]) -> Res<&[u8], Circuit> {
     let (bytes, _) = terminated(many0(parse_usize), newline)(bytes)?;
     // The rest of the file is gate definitions
     let (bytes, gates) = separated_list0(newline, parse_gate)(bytes)?;
-
+    // Optional blank lines at end
+    let (bytes, _) = all_consuming(many0(newline))(bytes)?;
+    assert_eq!(number_of_gates, gates.len()); // TODO return Err or move to TryFrom
     Ok((
         bytes,
         Circuit {
-            number_of_gates,
-            number_of_wires,
+            num_gates: number_of_gates,
+            num_wires: number_of_wires,
+            num_inputs: 2, // TODO
+            input_wires: vec![128, 128],
+            num_outputs: 1, // TODO
+            output_wires: vec![128],
             gates,
         },
     ))
-}
-
-fn parse_usize(line: &[u8]) -> Res<&[u8], usize> {
-    let (line, res) = delimited(space0, u64, space0)(line)?;
-    Ok((line, res as usize))
 }
 
 fn parse_gate(line: &[u8]) -> Res<&[u8], Gate> {
@@ -83,19 +161,41 @@ fn parse_kind(line: &[u8]) -> Res<&[u8], GateKind> {
     ))(line)
 }
 
+fn parse_usize(line: &[u8]) -> Res<&[u8], usize> {
+    let (line, res) = delimited(space0, u64, space0)(line)?;
+    Ok((line, res as usize))
+}
+
+
+
 #[cfg(test)]
 mod tests {
+    use crate::computation::{HostPlacement, Role};
+    use crate::kernels::SyncSession;
+    use crate::host::HostBitTensor;
+
     use super::*;
 
     #[test]
     fn test_parse_aes() {
-        let circuit = parse_circuit(crate::circuits::AES_128);
-        println!("Parsed circuit: {:?}", circuit);
+        let circuit = Circuit::try_from(AES_128).unwrap();
+        // println!("Parsed circuit: {:?}", circuit);
     }
 
     #[test]
-    fn test_parse_gate() {
-        let gate = parse_gate("2 1 33280 33282 3691 XOR".as_bytes());
-        println!("Parsed gate: {:?}", gate);
+    fn test_run_aes() {
+        let k: Vec<u8> = vec![0; 128];
+        let m: Vec<u8> = vec![0; 128];
+
+        let plc = HostPlacement { owner: Role::from("alice") };
+
+        let k: Vec<HostBitTensor> = k.iter().map(|b| HostBitTensor::from_slice_plc(&[*b], plc.clone())).collect();
+        let m: Vec<HostBitTensor> = m.iter().map(|b| HostBitTensor::from_slice_plc(&[*b], plc.clone())).collect();
+
+        let sess = SyncSession::default();
+
+        let c = aes(&sess, &plc, k, m);
+        let c_bits: Vec<u8> = c.iter().map(|t| t.0[0] & 1).collect();
+        println!("{:?}", c_bits);
     }
 }
--- moose/src/circuits/mod.rs
+++ moose/src/circuits/mod.rs
@@ -1,3 +1,1 @@
 pub mod bristol_fashion;
-
-pub const AES_128: &[u8] = include_bytes!("aes_128.txt");
