4274925a63279f66acdf26845b5a3bb0e200a732
Dragos Rotaru <r.dragos0@gmail.com>
2021-11-17 17:22:16 +0200

    cleanup

--- moose/src/replicated/mod.rs
+++ moose/src/replicated/mod.rs
@@ -21,7 +21,6 @@ pub mod aes;
 pub mod control_flow;
 pub mod division;
 pub mod exp;
-
 pub mod input;
 pub mod log;
 pub use self::aes::ReplicatedAesKey;
@@ -1270,7 +1269,6 @@ modelled!(PlacementMul::mul, ReplicatedPlacement, (HostRing128Tensor, Replicated
 modelled!(PlacementMul::mul, ReplicatedPlacement, (ReplicatedRing64Tensor, HostRing64Tensor) -> ReplicatedRing64Tensor, RepMulOp);
 modelled!(PlacementMul::mul, ReplicatedPlacement, (ReplicatedRing128Tensor, HostRing128Tensor) -> ReplicatedRing128Tensor, RepMulOp);
 modelled!(PlacementMul::mul, ReplicatedPlacement, (ReplicatedBitTensor, ReplicatedBitTensor) -> ReplicatedBitTensor, RepMulOp);
-
 modelled!(PlacementMul::mul, ReplicatedPlacement, (ReplicatedRing128Tensor, Mirrored3Ring128Tensor) -> ReplicatedRing128Tensor, RepMulOp);
 modelled!(PlacementMul::mul, ReplicatedPlacement, (Mirrored3Ring128Tensor, ReplicatedRing128Tensor) -> ReplicatedRing128Tensor, RepMulOp);
 modelled!(PlacementMul::mul, ReplicatedPlacement, (ReplicatedRing64Tensor, Mirrored3Ring64Tensor) -> ReplicatedRing64Tensor, RepMulOp);
@@ -2540,7 +2538,7 @@ impl ShapeOp {
     }
 }
 
-pub trait BinaryAdder<S: Session, RepBitT> {
+pub(crate) trait BinaryAdder<S: Session, RepBitT> {
     fn binary_adder(&self, sess: &S, x: &RepBitT, y: &RepBitT, ring_size: usize) -> RepBitT;
 }
 
