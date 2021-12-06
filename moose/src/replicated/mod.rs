396e25ab91b8e217ec8720ede5fb85576e8883da
Morten Dahl <mortendahlcs@gmail.com>
2021-12-06 15:20:28 +0100

    simplify bounds

--- moose/src/replicated/mod.rs
+++ moose/src/replicated/mod.rs
@@ -1681,8 +1681,8 @@ impl RepSumOp {
 modelled_kernel! {
     PlacementTruncPr::trunc_pr, RepTruncPrOp{amount: u32},
     [
-        (ReplicatedPlacement,  (ReplicatedRing64Tensor) -> ReplicatedRing64Tensor => [hybrid] Self::kernel),
-        (ReplicatedPlacement,  (ReplicatedRing128Tensor) -> ReplicatedRing128Tensor => [hybrid] Self::kernel),
+        (ReplicatedPlacement,  (ReplicatedRing64Tensor) -> ReplicatedRing64Tensor => [concrete] Self::kernel),
+        (ReplicatedPlacement,  (ReplicatedRing128Tensor) -> ReplicatedRing128Tensor => [concrete] Self::kernel),
     ]
 }
 
@@ -1692,23 +1692,11 @@ impl RepTruncPrOp {
         rep: &ReplicatedPlacement,
         amount: u32,
         xe: RepTen<HostRingT>,
-    ) -> Result<m!(c!(RepTen<HostRingT>))>
+    ) -> Result<RepTen<HostRingT>>
     where
-        RepTen<HostRingT>: Clone,
-
-        RepTen<HostRingT>: CanonicalType,
-        <RepTen<HostRingT> as CanonicalType>::Type: KnownType<S>,
-
-        AdtTen<HostRingT>: CanonicalType,
-        <AdtTen<HostRingT> as CanonicalType>::Type: KnownType<S>,
-
-        RepTen<HostRingT>: Into<st!(RepTen<HostRingT>)>,
-        st!(AdtTen<HostRingT>): TryInto<AdtTen<HostRingT>>,
-        AdtTen<HostRingT>: Into<st!(AdtTen<HostRingT>)>,
-
-        AdditivePlacement: PlacementRepToAdt<S, st!(RepTen<HostRingT>), st!(AdtTen<HostRingT>)>,
+        AdditivePlacement: PlacementRepToAdt<S, RepTen<HostRingT>, AdtTen<HostRingT>>,
         AdditivePlacement: PlacementTruncPrProvider<S, AdtTen<HostRingT>, AdtTen<HostRingT>>,
-        ReplicatedPlacement: PlacementAdtToRep<S, st!(AdtTen<HostRingT>), st!(RepTen<HostRingT>)>,
+        ReplicatedPlacement: PlacementAdtToRep<S, AdtTen<HostRingT>, RepTen<HostRingT>>,
     {
         let (player0, player1, player2) = rep.host_placements();
 
@@ -1717,9 +1705,9 @@ impl RepTruncPrOp {
         };
         let provider = player2;
 
-        let x_adt = adt.rep_to_adt(sess, &xe.into()).try_into().ok().unwrap();
+        let x_adt = adt.rep_to_adt(sess, &xe);
         let y_adt = adt.trunc_pr(sess, amount as usize, &provider, &x_adt);
-        Ok(rep.adt_to_rep(sess, &y_adt.into()))
+        Ok(rep.adt_to_rep(sess, &y_adt))
     }
 }
 
