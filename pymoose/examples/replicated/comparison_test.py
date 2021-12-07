592acab427bcf5feac74820cddd1ce66abeb77ab
Dragos Rotaru <r.dragos0@gmail.com>
2021-12-07 15:23:08 +0200

    refactored edsl comparison test

--- pymoose/examples/replicated/comparison_test.py
+++ pymoose/examples/replicated/comparison_test.py
@@ -43,13 +43,6 @@ class ReplicatedExample(parameterized.TestCase):
         less_comp = self._setup_less_comp()
         traced_less_comp = edsl.trace(less_comp)
         comp_bin = utils.serialize_computation(traced_less_comp)
-        deser_less_comp = utils.deserialize_computation(comp_bin)
-        assert traced_less_comp == deser_less_comp
-
-    def test_less_example_rust_serde(self):
-        less_comp = self._setup_less_comp()
-        traced_less_comp = edsl.trace(less_comp)
-        comp_bin = utils.serialize_computation(traced_less_comp)
         # Compile in Rust
         # If this does not error, rust was able to deserialize the pycomputation
         elk_compiler.compile_computation(comp_bin, [])
@@ -71,26 +64,14 @@ class ReplicatedExample(parameterized.TestCase):
     def test_less_example_execute(self):
         less_comp = self._setup_less_comp()
         traced_less_comp = edsl.trace(less_comp)
-        comp_bin = utils.serialize_computation(traced_less_comp)
-        compiled_comp = elk_compiler.compile_computation(
-            comp_bin,
-            [
-                "typing",
-                "full",
-                "prune",
-                "networking",
-                "toposort",
-                # "print",
-            ],
-        )
         storage = {
             "alice": {},
             "bob": {},
             "carole": {},
         }
         runtime = LocalMooseRuntime(storage_mapping=storage)
-        comp_result = runtime.evaluate_compiled(
-            comp_bin=compiled_comp,
+        comp_result = runtime.evaluate_computation(
+            computation=traced_less_comp,
             role_assignment={"alice": "alice", "bob": "bob", "carole": "carole"},
             arguments={},
         )
