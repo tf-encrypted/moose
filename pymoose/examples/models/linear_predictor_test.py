daddf09398dd263a0419345d94882753144fd6b2
jvmncs <jvmncs@gmail.com>
2022-01-07 12:28:16 -0500

    Test various sklearn regression against LinearPredictor model (#751)
    * add onnx to dev dependencies
    
    * add test fixtures for all relevant sklearn linear regression models
    
    * test against all sklearn regression fixtures
    
    * sort sk_regression_models list alphabetically

--- pymoose/examples/models/linear_predictor_test.py
+++ pymoose/examples/models/linear_predictor_test.py
@@ -13,18 +13,42 @@ from pymoose.computation import utils as comp_utils
 from . import linear_predictor
 from . import model_utils
 
+_SK_REGRESSION_MODELS = [
+    "ard_regression",
+    "bayesian_ridge",
+    "elastic_net",
+    "elastic_net_cv",
+    "huber_regressor",
+    "lars",
+    "lars_cv",
+    "lasso",
+    "lasso_cv",
+    "lasso_lars_ic",
+    "linear_regression",
+    "orthogonal_matching_pursuit",
+    "orthogonal_matching_pursuit_cv",
+    "passive_aggressive_regressor",
+    "quantile_regressor",
+    "ransac_regressor",
+    "ridge",
+    "ridge_cv",
+    "sgd_regressor",
+    "theil_sen_regressor",
+]
+
 
 class LinearPredictorTest(parameterized.TestCase):
-    def _build_linear_predictor(self):
+    def _build_linear_predictor(self, onnx_fixture):
         root_path = pathlib.Path(__file__).parent.absolute()
-        fixture_path = root_path / "fixtures" / "linear_regressor.onnx"
+        fixture_path = root_path / "fixtures" / f"{onnx_fixture}.onnx"
         with open(fixture_path, "rb") as model_fixture:
             lr_onnx = onnx.load_model(model_fixture)
         linear_model = linear_predictor.LinearPredictor.from_onnx_proto(lr_onnx)
         return linear_model
 
-    def test_predictor_logic(self):
-        linear_model = self._build_linear_predictor()
+    @parameterized.parameters(*_SK_REGRESSION_MODELS)
+    def test_predictor_logic(self, model_name):
+        linear_model = self._build_linear_predictor(model_name)
 
         @edsl.computation
         def predictor_minus_aes(
@@ -54,8 +78,9 @@ class LinearPredictorTest(parameterized.TestCase):
         expected_result = linear_model.coeffs.sum() + linear_model.intercept
         np.testing.assert_almost_equal(actual_result, expected_result)
 
-    def test_serde(self):
-        linear_model = self._build_linear_predictor()
+    @parameterized.parameters(*_SK_REGRESSION_MODELS)
+    def test_serde(self, model_name):
+        linear_model = self._build_linear_predictor(model_name)
         predictor = linear_model.predictor_factory()
         traced_predictor = edsl.trace(predictor)
         serialized = comp_utils.serialize_computation(traced_predictor)
