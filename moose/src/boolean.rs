c1087f9ca6126685be338102f2fb5f5e8de35dc2
Morten Dahl <mortendahlcs@gmail.com>
2022-01-12 14:06:43 +0100

    Boolean docs

--- moose/src/boolean.rs
+++ moose/src/boolean.rs
@@ -1,3 +1,5 @@
+//! Logical boolean tensors
+
 use crate::computation::*;
 use crate::error::{Error, Result};
 use crate::host::HostBitTensor;
@@ -6,7 +8,8 @@ use crate::kernels::*;
 use crate::replicated::{ReplicatedBitTensor, ReplicatedPlacement};
 use serde::{Deserialize, Serialize};
 
-/// TODO(Dragos) perhaps we can unify BoolTensor with FixedTensor
+/// Boolean tensor abstracting over host and replicated values
+// TODO(Dragos) perhaps we can unify BoolTensor with FixedTensor
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum BoolTensor<HostT, RepT> {
     Host(HostT),
