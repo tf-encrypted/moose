358adbc960f9ce9e0f60ba2a13fe1b9c6e2a2469
jvmncs <jvmncs@gmail.com>
2022-01-21 18:02:47 -0500

    move a fixedpoint constant to mirrored from bob

--- pymoose/pymoose/predictors/tree_ensemble.py
+++ pymoose/pymoose/predictors/tree_ensemble.py
@@ -267,7 +267,7 @@ class TreeEnsembleClassifier(TreeEnsemble):
         logit = edsl.add(edsl.add_n(tree_scores), base_score)
         pos_prob = edsl.sigmoid(logit) if self.transform_output else logit
         pos_prob = edsl.expand_dims(pos_prob, axis=1)
-        one = self.fixedpoint_constant(1, plc=self.bob, dtype=fixedpoint_dtype)
+        one = self.fixedpoint_constant(1, plc=self.mirrored, dtype=fixedpoint_dtype)
         neg_prob = edsl.sub(one, pos_prob)
         return edsl.concatenate([neg_prob, pos_prob], axis=1)
 
