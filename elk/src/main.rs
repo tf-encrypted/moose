8aaa4bcd6b74a3c22a78900ca1ef1e3b599ed84d
Lex Vorona <lex@capeprivacy.com>
2022-02-16 09:34:41 -0800

    Switch elk to the clap and subcommands

--- elk/src/main.rs
+++ elk/src/main.rs
@@ -1,37 +1,52 @@
+use clap::{Parser, Subcommand};
 use moose::compilation::compile;
 use moose::textual::verbose_parse_computation;
 use moose::textual::ToTextual;
 use std::fs::{read_to_string, write};
 use std::path::PathBuf;
 
-use structopt::StructOpt;
+#[derive(Parser)]
+#[clap(name = "elk")]
+#[clap(
+    about = "A Moose compiler wrapper CLI",
+    long_about = "Takes an input file with the Computation's Textual representation and applies the specified passes to it."
+)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
 
-#[derive(Debug, StructOpt)]
-/// A Moose compiler wrapper CLI
-///
-/// Takes an input file with the Computation's Textual representation and applies the specified passes to it.
-struct Opt {
-    /// Input file
-    #[structopt(parse(from_os_str))]
-    input: PathBuf,
+#[derive(Subcommand)]
+enum Commands {
+    #[clap(arg_required_else_help = true)]
+    Compile {
+        /// Input file
+        input: PathBuf,
 
-    /// Output file, stdout if not present
-    #[structopt(parse(from_os_str))]
-    output: Option<PathBuf>,
+        /// Output file, stdout if not present
+        output: Option<PathBuf>,
 
-    /// List of passes to apply. In order. Default to run all the passes
-    #[structopt(short, long)]
-    passes: Option<Vec<String>>,
+        /// List of passes to apply. In order. Default to run all the passes
+        passes: Option<Vec<String>>,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
-    let opt = Opt::from_args();
-    let source = read_to_string(opt.input)?;
-    let comp = verbose_parse_computation(&source)?;
-    let comp = compile(&comp, opt.passes)?;
-    match opt.output {
-        Some(path) => write(path, comp.to_textual())?,
-        None => println!("{}", comp.to_textual()),
+    let args = Cli::parse();
+    match &args.command {
+        Commands::Compile {
+            input,
+            output,
+            passes,
+        } => {
+            let source = read_to_string(input)?;
+            let comp = verbose_parse_computation(&source)?;
+            let comp = compile(&comp, passes)?;
+            match output {
+                Some(path) => write(path, comp.to_textual())?,
+                None => println!("{}", comp.to_textual()),
+            }
+        }
     }
     Ok(())
 }
