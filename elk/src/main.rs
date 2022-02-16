a64c748b1d8badb491480a21dfa9d8a292f4d446
Lex Vorona <lex@capeprivacy.com>
2022-02-16 09:47:27 -0800

    Actually use subcommands

--- elk/src/main.rs
+++ elk/src/main.rs
@@ -5,7 +5,7 @@ use moose::textual::ToTextual;
 use std::fs::{read_to_string, write};
 use std::path::PathBuf;
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[clap(name = "elk")]
 #[clap(
     about = "A Moose compiler wrapper CLI",
@@ -16,9 +16,9 @@ struct Cli {
     command: Commands,
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 enum Commands {
-    #[clap(arg_required_else_help = true)]
+    /// Compiles the moose computation
     Compile {
         /// Input file
         input: PathBuf,
@@ -27,7 +27,16 @@ enum Commands {
         output: Option<PathBuf>,
 
         /// List of passes to apply. In order. Default to run all the passes
+        #[clap(short, long, required = true)]
         passes: Option<Vec<String>>,
+    },
+    /// Prints stats about a computation without transforming it
+    Stats {
+        /// Input file
+        input: PathBuf,
+
+        /// The kind of the stats to produce
+        flavour: String,
     },
 }
 
@@ -41,11 +50,16 @@ fn main() -> anyhow::Result<()> {
         } => {
             let source = read_to_string(input)?;
             let comp = verbose_parse_computation(&source)?;
-            let comp = compile(&comp, passes)?;
+            let comp = compile(&comp, passes.clone())?;
             match output {
                 Some(path) => write(path, comp.to_textual())?,
                 None => println!("{}", comp.to_textual()),
             }
+        }
+        Commands::Stats { input, flavour } => {
+            println!("Computing {}", flavour);
+            let source = read_to_string(input)?;
+            let _comp = verbose_parse_computation(&source)?;
         }
     }
     Ok(())
