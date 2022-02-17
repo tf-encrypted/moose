c149ec2fc56318457db597f0992d57f41b87d41a
Morten Dahl <mortendahlcs@gmail.com>
2022-02-17 16:17:06 +0100

    update worker

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,7 +1,7 @@
 use moose::prelude::*;
 use moose::storage::LocalAsyncStorage;
-use moose_choreography::choreography::filesystem::FilesystemChoreography;
-use moose_networking::networking::grpc::GrpcNetworkingManager;
+use moose_modules::choreography::filesystem::FilesystemChoreography;
+use moose_modules::networking::grpc::GrpcNetworkingManager;
 use std::sync::Arc;
 use structopt::StructOpt;
 use tonic::transport::Server;
