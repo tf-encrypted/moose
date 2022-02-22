321ad42ec3fd4ad7707699069ce6fca9d6190851
Kyle Kloberdanz <kyle@capeprivacy.com>
2022-02-22 13:51:15 -0600

    replace anyhow results with moose result/error

--- modules/src/networking/tcpstream.rs
+++ modules/src/networking/tcpstream.rs
@@ -4,6 +4,7 @@ use moose::{
     error::Error,
     execution::Identity,
     networking::AsyncNetworking,
+    Result,
 };
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
@@ -40,7 +41,7 @@ fn little_endian_to_u64(buf: &[u8; 8]) -> u64 {
     n
 }
 
-async fn handle_connection(mut stream: TcpStream, store: StoreType) -> anyhow::Result<()> {
+async fn handle_connection(mut stream: TcpStream, store: StoreType) -> Result<()> {
     loop {
         // read moose data
         let mut buf: [u8; 8] = [0; 8];
@@ -55,9 +56,11 @@ async fn handle_connection(mut stream: TcpStream, store: StoreType) -> anyhow::R
         let mut vec: Vec<u8> = vec![0; size as usize];
 
         tracing::debug!("reading exact: {}", size);
-        stream.read_exact(&mut vec).await?;
+        stream.read_exact(&mut vec).await.map_err(|e| {
+            Error::Networking(format!("failed to read data from TCP stream: {}", e))
+        })?;
         let data: SendData = bincode::deserialize(&vec)
-            .map_err(|e| anyhow::anyhow!("failed to deserialize moose value: {}", e))?;
+            .map_err(|e| Error::Networking(format!("failed to deserialize moose value: {}", e)))?;
 
         // put value into store
         let key = (data.session_id, data.rendezvous_key);
@@ -73,11 +76,14 @@ async fn handle_connection(mut stream: TcpStream, store: StoreType) -> anyhow::R
     }
 }
 
-async fn server(listener: TcpListener, store: StoreType) -> anyhow::Result<()> {
+async fn server(listener: TcpListener, store: StoreType) -> Result<()> {
     loop {
         // TODO: retry logic?
         tracing::debug!("listening");
-        let (stream, addr) = listener.accept().await?;
+        let (stream, addr) = listener
+            .accept()
+            .await
+            .map_err(|e| Error::Networking(format!("failed to accept connection: {}", e)))?;
         tracing::debug!("accepted connection: {}", addr);
         let shared_store = Arc::clone(&store);
         tokio::spawn(async move {
@@ -94,28 +100,46 @@ struct SendData {
     session_id: SessionId,
 }
 
-async fn send_value(stream: &mut TcpStream, send_data: &SendData) -> anyhow::Result<()> {
-    let raw_data: Vec<u8> = bincode::serialize(send_data)?;
+async fn send_value(stream: &mut TcpStream, send_data: &SendData) -> Result<()> {
+    let raw_data: Vec<u8> = bincode::serialize(send_data)
+        .map_err(|e| Error::Networking(format!("could not serialze send_data: {}", e)))?;
     let data_size = raw_data.len();
     let mut size_data_buf = [0; 8];
-    u64_to_little_endian(data_size.try_into()?, &mut size_data_buf);
+    u64_to_little_endian(
+        data_size
+            .try_into()
+            .map_err(|e| Error::Networking(format!("could not cast data_size: {}", e)))?,
+        &mut size_data_buf,
+    );
 
     // TODO: write error, re-establish connection?
-    stream.write_all(&size_data_buf).await?;
-    stream.write_all(&raw_data).await?;
-    stream.flush().await?;
+    stream.write_all(&size_data_buf).await.map_err(|e| {
+        Error::Networking(format!("could not write data size over TCP stream: {}", e))
+    })?;
+    stream
+        .write_all(&raw_data)
+        .await
+        .map_err(|e| Error::Networking(format!("could not write data over TCP stream: {}", e)))?;
+    stream
+        .flush()
+        .await
+        .map_err(|e| Error::Networking(format!("could not flush TCP stream: {}", e)))?;
     Ok(())
 }
 
-async fn send_loop(mut stream: TcpStream, mut rx: mpsc::Receiver<SendData>) -> anyhow::Result<()> {
+async fn send_loop(mut stream: TcpStream, mut rx: mpsc::Receiver<SendData>) -> Result<()> {
     loop {
         let send_data = rx.recv().await;
         match send_data {
             Some(data) => {
-                send_value(&mut stream, &data).await?;
+                send_value(&mut stream, &data).await.map_err(|e| {
+                    Error::Networking(format!("could not send value to other worker: {}", e))
+                })?;
             }
             None => {
-                stream.shutdown().await?;
+                stream.shutdown().await.map_err(|e| {
+                    Error::Networking(format!("failed to shutdown TCP stream: {}", e))
+                })?;
                 return Ok(());
             }
         }
@@ -126,17 +150,19 @@ impl TcpStreamNetworking {
     pub async fn new(
         own_name: &str,
         hosts: HashMap<String, String>,
-    ) -> anyhow::Result<TcpStreamNetworking> {
+    ) -> Result<TcpStreamNetworking> {
         tracing::debug!("own name: {}", own_name);
         let store = StoreType::default();
         let own_name: String = own_name.to_string();
         let own_address = hosts
             .get(&own_name)
-            .ok_or_else(|| anyhow::anyhow!("own host name not in hosts map"))?;
+            .ok_or_else(|| Error::Networking("own host name not in hosts map".to_string()))?;
 
         // spawn the server
         tracing::debug!("spawned server on: {}", own_address);
-        let listener = TcpListener::bind(&own_address).await?;
+        let listener = TcpListener::bind(&own_address).await.map_err(|e| {
+            Error::Networking(format!("could not bind to address: {}: {}", own_address, e))
+        })?;
         let shared_store = Arc::clone(&store);
         tokio::spawn(async move {
             server(listener, Arc::clone(&shared_store)).await.unwrap();
