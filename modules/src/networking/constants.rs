9bf774fd485807f62fc3db49a98b9721f323adf6
Dragos Rotaru <r.dragos0@gmail.com>
2022-03-09 20:36:42 +0200

    make clippy happy

--- modules/src/networking/constants.rs
+++ modules/src/networking/constants.rs
@@ -3,6 +3,6 @@
 /// The default multiplier value
 pub(crate) const MULTIPLIER: f64 = 1.;
 /// The default maximum back off time in milliseconds (3600 seconds).
-pub(crate) const MAX_INTERVAL_MILLIS: u64 = 3600_000;
+pub(crate) const MAX_INTERVAL_MILLIS: u64 = 3_600_000;
 /// The default maximum elapsed time in milliseconds (1 minute).
 pub(crate) const MAX_ELAPSED_TIME_MILLIS: u64 = 1_000;
