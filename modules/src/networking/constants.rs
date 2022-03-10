b20a1328f1b51bad6fdd6a9c3dc8d18333e09d76
Lex Vorona <lex@capeprivacy.com>
2022-03-10 14:54:37 -0800

    Make the retry time to be one minute

--- modules/src/networking/constants.rs
+++ modules/src/networking/constants.rs
@@ -7,7 +7,7 @@ pub(crate) const MULTIPLIER: f64 = 1.;
 
 lazy_static! {
     /// The default maximum elapsed time in milliseconds (1 minute).
-    pub(crate) static ref MAX_ELAPSED_TIME: Option<Duration> = Some(Duration::from_millis(1_000));
+    pub(crate) static ref MAX_ELAPSED_TIME: Option<Duration> = Some(Duration::from_millis(60_000));
     /// The default maximum back off time in milliseconds (3600 seconds).
     pub(crate) static ref MAX_INTERVAL: Duration = Duration::from_millis(3_600_000);
 }
