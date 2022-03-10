3700270fbd8b7bc9a62b2fe09fadab092e84b1f0
Kyle Kloberdanz <kyle@capeprivacy.com>
2022-03-10 15:32:31 -0600

    added numpy storage

--- modules/src/storage/numpy.rs
+++ modules/src/storage/numpy.rs
@@ -1,29 +1,54 @@
 use moose::prelude::*;
-use ndarray::{Array2, ShapeBuilder};
+use moose::Result;
+use ndarray::ArrayD;
+use ndarray_npy::read_npy;
 use std::io::Read;
 
-pub async fn read_numpy(
-    filename: &str,
-    _maybe_type_hint: Option<Ty>,
-    _columns: &[String],
-    _placement: &str,
-) -> anyhow::Result<Value> {
+pub async fn read_numpy(filename: &str, placement: &HostPlacement) -> Result<Value> {
     let mut buf = vec![];
     std::fs::File::open(filename)
         .unwrap()
         .read_to_end(&mut buf)
         .unwrap();
-    unimplemented!("numpy");
+    let arr: ArrayD<f64> = read_npy(filename).unwrap();
+    let tensor: HostFloat64Tensor = placement.from_raw(arr);
+    let value = Value::from(tensor);
+    Ok(value)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ndarray::array;
+    use std::io::Write;
+    use tempfile::NamedTempFile;
 
     #[tokio::test]
     async fn test_read_numpy() {
-        let path = std::env::current_dir().unwrap();
-        println!("The current directory is {}", path.display());
-        read_numpy("data.npy", None, &[], "host").await.unwrap();
+        let plc = HostPlacement::from("host");
+        let tensor: HostFloat64Tensor = plc.from_raw(array![
+            [[2.3, 4.0, 5.0], [6.0, 7.0, 12.0]],
+            [[8.0, 9.0, 14.0], [10.0, 11.0, 16.0]]
+        ]);
+        let expected = Value::from(tensor);
+        let mut file = NamedTempFile::new().expect("trying to create tempfile");
+        let path = file.path();
+        let filename = path
+            .to_str()
+            .expect("trying to get path from temp file")
+            .to_string();
+
+        let file_data = concat!(
+            "k05VTVBZAQB2AHsnZGVzY3InOiAnPGY4JywgJ2ZvcnRyYW5fb3JkZXInOiBGYWxzZSwgJ3NoYXBl",
+            "JzogKDIsIDIsIDMpLCB9ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg",
+            "ICAgICAgICAgICAgIApmZmZmZmYCQAAAAAAAABBAAAAAAAAAFEAAAAAAAAAYQAAAAAAAABxAAAAA",
+            "AAAAKEAAAAAAAAAgQAAAAAAAACJAAAAAAAAALEAAAAAAAAAkQAAAAAAAACZAAAAAAAAAMEA="
+        );
+        let raw_bytes = base64::decode(file_data).unwrap();
+        file.write_all(&raw_bytes).unwrap();
+
+        let plc = HostPlacement::from("host");
+        let data = read_numpy(&filename, &plc).await.unwrap();
+        assert_eq!(data, expected);
     }
 }
