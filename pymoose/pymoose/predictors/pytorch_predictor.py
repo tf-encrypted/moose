6f5836a1af69a1b327dd4ee81352ec8b3d2065e8
ekloberdanz <kloberdanz.eliska@gmail.com>
2022-03-17 14:31:13 -0500

    prevent class abstraction leakage - return only activation functions from onnx parser

--- pymoose/pymoose/predictors/pytorch_predictor.py
+++ pymoose/pymoose/predictors/pytorch_predictor.py
@@ -9,11 +9,11 @@ from pymoose.predictors import predictor_utils
 
 
 class NeuralNetwork(aes_predictor.AesPredictor, metaclass=abc.ABCMeta):
-    def __init__(self, weights, biases, operations):
+    def __init__(self, weights, biases, activations):
         super().__init__()
         self.weights = weights
         self.biases = biases
-        self.operations = operations
+        self.activations = activations
         self.n_classes = np.shape(biases[-1])[0]  # infer number of classes
 
     def apply_layer(self, input, i, fixedpoint_dtype):
@@ -27,8 +27,9 @@ class NeuralNetwork(aes_predictor.AesPredictor, metaclass=abc.ABCMeta):
         z = edsl.add(y, b)
         return z
 
-    def activation_fn(self, z, op):
-        if op == "Sigmoid":
+    def activation_fn(self, z, i):
+        activation = self.activations[i]
+        if activation == "Sigmoid":
             activation_output = edsl.sigmoid(z)
         # There is a bug in edsl.shape
         # elif op == "Relu":
@@ -37,20 +38,19 @@ class NeuralNetwork(aes_predictor.AesPredictor, metaclass=abc.ABCMeta):
         #     ones = edsl.cast(ones, dtype=predictor_utils.DEFAULT_FIXED_DTYPE)
         #     zeros = edsl.sub(ones, ones)
         #     activation_output = edsl.maximum([zeros, z])
-        elif op == "Softmax":
+        elif activation == "Softmax":
             activation_output = edsl.softmax(z, axis=1, upmost_index=self.n_classes)
+        elif activation == "Identity":
+            activation_output = z
         else:
             raise ValueError("Invalid or unsupported activation function")
         return activation_output
 
     def neural_predictor_fn(self, x, fixedpoint_dtype):
-        dense_layer_position = 0
-        for op in self.operations:
-            if op == "Gemm":
-                x = self.apply_layer(x, dense_layer_position, fixedpoint_dtype)
-                dense_layer_position += 1
-            else:
-                x = self.activation_fn(x, op)
+        num_layers = len(self.weights)
+        for i in range(num_layers):
+            x = self.apply_layer(x, i, fixedpoint_dtype)
+            x = self.activation_fn(x, i)
         return x
 
     def predictor_factory(self, fixedpoint_dtype=predictor_utils.DEFAULT_FIXED_DTYPE):
@@ -70,8 +70,15 @@ class NeuralNetwork(aes_predictor.AesPredictor, metaclass=abc.ABCMeta):
 
     @classmethod
     def from_onnx(cls, model_proto):
+        # extract activations from operations
         operations = predictor_utils.find_op_types_in_model_proto(model_proto)
-
+        activations = []
+        for i in range(len(operations)):
+            if operations[i] != "Gemm":
+                activations.append(operations[i])
+            if i > 0:
+                if operations[i] == "Gemm" and operations[i - 1] == "Gemm":
+                    activations.append("Identity")
         weights_data = predictor_utils.find_parameters_in_model_proto(
             model_proto, "weight", enforce=False
         )
@@ -106,4 +113,4 @@ class NeuralNetwork(aes_predictor.AesPredictor, metaclass=abc.ABCMeta):
             bias = np.asarray(bias)
             biases.append(bias)
 
-        return cls(weights, biases, operations)
+        return cls(weights, biases, activations)
