bfdbdc3cb8f3fbdb6e7740e0594784c3e4bb6ad7
ekloberdanz <kloberdanz.eliska@gmail.com>
2022-03-22 09:12:34 -0500

    Revert "refactored classes per Jason's request"
    This reverts commit 2d1ff25d0f005974a695f13a8919e9214c378bad.

--- pymoose/pymoose/predictors/neural_net_predictor.py
+++ pymoose/pymoose/predictors/neural_net_predictor.py
@@ -1,5 +1,4 @@
 import abc
-import struct
 
 import numpy as np
 
@@ -8,12 +7,12 @@ from pymoose.predictors import aes_predictor
 from pymoose.predictors import predictor_utils
 
 
-class NeuralNetwork(aes_predictor.AesPredictor, metaclass=abc.ABCMeta):
-    def __init__(self, weights, biases, activations):
+class MLPPredictor(aes_predictor.AesPredictor, metaclass=abc.ABCMeta):
+    def __init__(self, weights, biases, activation):
         super().__init__()
         self.weights = weights
         self.biases = biases
-        self.activations = activations
+        self.activation = activation
 
     @classmethod
     @abc.abstractmethod
@@ -21,74 +20,12 @@ class NeuralNetwork(aes_predictor.AesPredictor, metaclass=abc.ABCMeta):
         pass
 
     @abc.abstractmethod
-    def apply_layer(self, input, i, fixedpoint_dtype):
+    def post_transform(self, y, fixedpoint_dtype):
         pass
 
-    @classmethod
-    @abc.abstractmethod
-    def activation_fn(self, z, i=None):
-        pass
-
-    @abc.abstractmethod
-    def predictor_factory(self, fixedpoint_dtype=predictor_utils.DEFAULT_FIXED_DTYPE):
-        pass
-
-    @abc.abstractmethod
-    def neural_predictor_fn(self, x, fixedpoint_dtype):
-        pass
-
-
-class FullyConnectedNeuralNetwork(NeuralNetwork):
-    @classmethod
-    def from_onnx(cls, model_proto):
-        # extract activations from operations
-        operations = predictor_utils.find_op_types_in_model_proto(model_proto)
-        activations = []
-        for i in range(len(operations)):
-            if operations[i] != "Gemm":
-                activations.append(operations[i])
-            if i > 0:
-                if operations[i] == "Gemm" and operations[i - 1] == "Gemm":
-                    activations.append("Identity")
-        weights_data = predictor_utils.find_parameters_in_model_proto(
-            model_proto, "weight", enforce=False
-        )
-        biases_data = predictor_utils.find_parameters_in_model_proto(
-            model_proto, "bias", enforce=False
-        )
-        weights = []
-        for weight in weights_data:
-            dimentions = weight.dims
-            assert weight is not None
-            if weight.data_type != 1:  # FLOATS
-                raise ValueError(
-                    "Neural Network Weights must be of type FLOATS, found other."
-                )
-            weight = weight.raw_data
-            # decode bytes object
-            weight = struct.unpack("f" * (dimentions[0] * dimentions[1]), weight)
-            weight = np.asarray(weight)
-            weight = weight.reshape(dimentions[0], dimentions[1]).T
-            weights.append(weight)
-
-        biases = []
-        for bias in biases_data:
-            dimentions = bias.dims
-            assert bias is not None
-            if bias.data_type != 1:  # FLOATS
-                raise ValueError(
-                    "Neural network biases must be of type FLOATS, found other."
-                )
-            bias = bias.raw_data
-            bias = struct.unpack("f" * dimentions[0], bias)
-            bias = np.asarray(bias)
-            biases.append(bias)
-
-        return cls(weights, biases, activations)
-
-    def apply_layer(self, input, i, fixedpoint_dtype):
+    def apply_layer(self, input, num_hidden_layers, i, fixedpoint_dtype):
         w = self.fixedpoint_constant(
-            self.weights[i], plc=self.mirrored, dtype=fixedpoint_dtype
+            self.weights[i].T, plc=self.mirrored, dtype=fixedpoint_dtype
         )
         b = self.fixedpoint_constant(
             self.biases[i], plc=self.mirrored, dtype=fixedpoint_dtype
@@ -97,35 +34,30 @@ class FullyConnectedNeuralNetwork(NeuralNetwork):
         z = edsl.add(y, b)
         return z
 
-    def activation_fn(self, z, i):
-        activation = self.activations[i]
-        if activation == "Sigmoid":
+    def activation_fn(self, z):
+        if self.activation == "Sigmoid":
             activation_output = edsl.sigmoid(z)
-        # There is a bug in edsl.shape
-        # elif op == "Relu":
-        #     z_shape = edsl.shape(edsl.cast(z, dtype=edsl.fixed(14, 23)))
-        #     ones = edsl.ones(z_shape, dtype=edsl.float64)
-        #     ones = edsl.cast(ones, dtype=predictor_utils.DEFAULT_FIXED_DTYPE)
-        #     zeros = edsl.sub(ones, ones)
-        #     activation_output = edsl.maximum([zeros, z])
-        elif activation == "Softmax":
-            n_classes = np.shape(self.biases[-1])[0]
-            activation_output = edsl.softmax(
-                z,
-                axis=1,
-                upmost_index=n_classes,  # infer number of classes or regression targets
-            )
-        elif activation == "Identity":
-            activation_output = z
+            # There is a bug in edsl.shape
+            # elif self.activation == "Relu":
+            #     y_1_shape = edsl.slice(edsl.shape(x), begin=0, end=1)
+            #     ones = edsl.ones(y_1_shape, dtype=edsl.float64)
+            #     ones = edsl.cast(ones, dtype=fixedpoint_dtype)
+            #     zeros = edsl.sub(ones, ones)
+            #     activation_output = edsl.maximum([zeros, y_1])
+        elif self.activation is None:
+            activation_output = z  # identity activation
         else:
             raise ValueError("Invalid or unsupported activation function")
         return activation_output
 
     def neural_predictor_fn(self, x, fixedpoint_dtype):
-        num_layers = len(self.weights)
-        for i in range(num_layers):
-            x = self.apply_layer(x, i, fixedpoint_dtype)
-            x = self.activation_fn(x, i)
+        num_hidden_layers = len(self.weights) - 1  # infer number of layers
+        for i in range(num_hidden_layers + 1):
+            x = self.apply_layer(x, num_hidden_layers, i, fixedpoint_dtype)
+            if i < num_hidden_layers:
+                x = self.activation_fn(x)
+            else:
+                x = x
         return x
 
     def predictor_factory(self, fixedpoint_dtype=predictor_utils.DEFAULT_FIXED_DTYPE):
@@ -139,15 +71,16 @@ class FullyConnectedNeuralNetwork(NeuralNetwork):
             x = self.handle_aes_input(aes_key, aes_data, decryptor=self.replicated)
             with self.replicated:
                 y = self.neural_predictor_fn(x, fixedpoint_dtype)
-            return self.handle_output(y, prediction_handler=self.bob)
+                pred = self.post_transform(y, fixedpoint_dtype)
+            return self.handle_output(pred, prediction_handler=self.bob)
 
         return predictor
 
 
-class MultiLayerPerceptron(NeuralNetwork):
-    @abc.abstractmethod
+class MLPRegressor(MLPPredictor):
     def post_transform(self, y, fixedpoint_dtype):
-        pass
+        # no-op for linear regression models
+        return y
 
     @classmethod
     def from_onnx(cls, model_proto):
@@ -184,83 +117,86 @@ class MultiLayerPerceptron(NeuralNetwork):
         )
         return cls(weights, biases, activation)
 
-    def apply_layer(self, input, i, fixedpoint_dtype):
-        w = self.fixedpoint_constant(
-            self.weights[i].T, plc=self.mirrored, dtype=fixedpoint_dtype
-        )
-        b = self.fixedpoint_constant(
-            self.biases[i], plc=self.mirrored, dtype=fixedpoint_dtype
-        )
-        y = edsl.dot(input, w)
-        z = edsl.add(y, b)
-        return z
 
-    def activation_fn(self, z):
-        if self.activations == "Sigmoid":
-            activation_output = edsl.sigmoid(z)
-            # There is a bug in edsl.shape
-            # elif self.activation == "Relu":
-            #     y_1_shape = edsl.slice(edsl.shape(x), begin=0, end=1)
-            #     ones = edsl.ones(y_1_shape, dtype=edsl.float64)
-            #     ones = edsl.cast(ones, dtype=fixedpoint_dtype)
-            #     zeros = edsl.sub(ones, ones)
-            #     activation_output = edsl.maximum([zeros, y_1])
-        elif self.activations is None:
-            activation_output = z  # identity activation
-        else:
-            raise ValueError("Invalid or unsupported activation function")
-        return activation_output
+class MLPClassifier(MLPPredictor):
+    def __init__(
+        self, weights, biases, n_classes, activation, transform_output=True,
+    ):
+        super().__init__(weights, biases, activation)
+        n_classes = n_classes
+        activation = activation
 
-    def neural_predictor_fn(self, x, fixedpoint_dtype):
-        num_hidden_layers = len(self.weights) - 1  # infer number of layers
-        for i in range(num_hidden_layers + 1):
-            x = self.apply_layer(x, i, fixedpoint_dtype)
-            if i < num_hidden_layers:
-                x = self.activation_fn(x)
-            else:
-                x = x
-        return x
-
-    def predictor_factory(self, fixedpoint_dtype=predictor_utils.DEFAULT_FIXED_DTYPE):
-        @edsl.computation
-        def predictor(
-            aes_data: edsl.Argument(
-                self.alice, vtype=edsl.AesTensorType(dtype=fixedpoint_dtype)
-            ),
-            aes_key: edsl.Argument(self.replicated, vtype=edsl.AesKeyType()),
-        ):
-            x = self.handle_aes_input(aes_key, aes_data, decryptor=self.replicated)
-            with self.replicated:
-                y = self.neural_predictor_fn(x, fixedpoint_dtype)
-                pred = self.post_transform(y, fixedpoint_dtype)
-            return self.handle_output(pred, prediction_handler=self.bob)
-
-        return predictor
-
-
-class MLPRegressor(MultiLayerPerceptron):
-    def post_transform(self, y, fixedpoint_dtype):
-        # no-op for linear regression models
-        return y
-
-
-class MLPClassifier(MultiLayerPerceptron):
-    def post_transform(self, y, fixedpoint_dtype):
         # infer post_transform
-        n_classes = np.shape(self.biases[-1])[0]
-        print("n_classes ", n_classes)
-        if n_classes == 1:
+        if n_classes == 2:
             self._post_transform = lambda x: self._sigmoid(
                 x, predictor_utils.DEFAULT_FIXED_DTYPE
             )
-            return self._post_transform(y)
-        elif n_classes > 1:
+        elif n_classes > 2:
             self._post_transform = lambda x: edsl.softmax(
                 x, axis=1, upmost_index=n_classes
             )
-            return self._post_transform(y)
         else:
             raise ValueError("Specify number of classes")
+
+    @classmethod
+    def from_onnx(cls, model_proto):
+        weights_data = predictor_utils.find_parameters_in_model_proto(
+            model_proto, "coefficient", enforce=False
+        )
+        biases_data = predictor_utils.find_parameters_in_model_proto(
+            model_proto, "intercepts", enforce=False
+        )
+        weights = []
+        for weight in weights_data:
+            dimentions = weight.dims
+            assert weight is not None
+            if weight.data_type != 1:  # FLOATS
+                raise ValueError(
+                    "MLP coefficients must be of type FLOATS, found other."
+                )
+            weight = np.asarray(weight.float_data)
+            weight = weight.reshape(dimentions).T
+            weights.append(weight)
+        biases = []
+        for bias in biases_data:
+            assert bias is not None
+            if bias.data_type != 1:  # FLOATS
+                raise ValueError(
+                    "MLP coefficients must be of type FLOATS, found other."
+                )
+            bias = np.asarray(bias.float_data)
+            biases.append(bias)
+
+        # parse labels
+        labels_node = predictor_utils.find_node_in_model_proto(
+            model_proto, "ZipMap", enforce=False
+        )
+        if labels_node is None:
+            raise ValueError(
+                "Incompatible ONNX graph provided: graph must contain a "
+                "labels_node operator."
+            )
+        classlabels_ints = predictor_utils.find_attribute_in_node(
+            labels_node, "classlabels_int64s", enforce=False
+        )
+        classlabels_strings = predictor_utils.find_attribute_in_node(
+            labels_node, "classlabels_strings", enforce=False
+        )
+        assert classlabels_ints is not None or classlabels_strings is not None
+        if classlabels_ints is not None:
+            classlabels = classlabels_ints.ints
+        elif classlabels_strings is not None:
+            classlabels = classlabels_strings.strings
+        n_classes = len(classlabels)
+
+        # parse activation function
+        activation = predictor_utils.find_activation_in_model_proto(
+            model_proto, "next_activations", enforce=False
+        )
+        return cls(weights, biases, n_classes, activation)
+
+    def post_transform(self, y, fixedpoint_dtype):
+        return self._post_transform(y)
 
     def _sigmoid(self, y, fixedpoint_dtype):
         """
---
+++ pymoose/pymoose/predictors/pytorch_predictor.py
@@ -0,0 +1,116 @@
+import abc
+import struct
+
+import numpy as np
+
+from pymoose import edsl
+from pymoose.predictors import aes_predictor
+from pymoose.predictors import predictor_utils
+
+
+class NeuralNetwork(aes_predictor.AesPredictor, metaclass=abc.ABCMeta):
+    def __init__(self, weights, biases, activations):
+        super().__init__()
+        self.weights = weights
+        self.biases = biases
+        self.activations = activations
+        self.n_classes = np.shape(biases[-1])[0]  # infer number of classes
+
+    def apply_layer(self, input, i, fixedpoint_dtype):
+        w = self.fixedpoint_constant(
+            self.weights[i], plc=self.mirrored, dtype=fixedpoint_dtype
+        )
+        b = self.fixedpoint_constant(
+            self.biases[i], plc=self.mirrored, dtype=fixedpoint_dtype
+        )
+        y = edsl.dot(input, w)
+        z = edsl.add(y, b)
+        return z
+
+    def activation_fn(self, z, i):
+        activation = self.activations[i]
+        if activation == "Sigmoid":
+            activation_output = edsl.sigmoid(z)
+        # There is a bug in edsl.shape
+        # elif op == "Relu":
+        #     z_shape = edsl.shape(edsl.cast(z, dtype=edsl.fixed(14, 23)))
+        #     ones = edsl.ones(z_shape, dtype=edsl.float64)
+        #     ones = edsl.cast(ones, dtype=predictor_utils.DEFAULT_FIXED_DTYPE)
+        #     zeros = edsl.sub(ones, ones)
+        #     activation_output = edsl.maximum([zeros, z])
+        elif activation == "Softmax":
+            activation_output = edsl.softmax(z, axis=1, upmost_index=self.n_classes)
+        elif activation == "Identity":
+            activation_output = z
+        else:
+            raise ValueError("Invalid or unsupported activation function")
+        return activation_output
+
+    def neural_predictor_fn(self, x, fixedpoint_dtype):
+        num_layers = len(self.weights)
+        for i in range(num_layers):
+            x = self.apply_layer(x, i, fixedpoint_dtype)
+            x = self.activation_fn(x, i)
+        return x
+
+    def predictor_factory(self, fixedpoint_dtype=predictor_utils.DEFAULT_FIXED_DTYPE):
+        @edsl.computation
+        def predictor(
+            aes_data: edsl.Argument(
+                self.alice, vtype=edsl.AesTensorType(dtype=fixedpoint_dtype)
+            ),
+            aes_key: edsl.Argument(self.replicated, vtype=edsl.AesKeyType()),
+        ):
+            x = self.handle_aes_input(aes_key, aes_data, decryptor=self.replicated)
+            with self.replicated:
+                y = self.neural_predictor_fn(x, fixedpoint_dtype)
+            return self.handle_output(y, prediction_handler=self.bob)
+
+        return predictor
+
+    @classmethod
+    def from_onnx(cls, model_proto):
+        # extract activations from operations
+        operations = predictor_utils.find_op_types_in_model_proto(model_proto)
+        activations = []
+        for i in range(len(operations)):
+            if operations[i] != "Gemm":
+                activations.append(operations[i])
+            if i > 0:
+                if operations[i] == "Gemm" and operations[i - 1] == "Gemm":
+                    activations.append("Identity")
+        weights_data = predictor_utils.find_parameters_in_model_proto(
+            model_proto, "weight", enforce=False
+        )
+        biases_data = predictor_utils.find_parameters_in_model_proto(
+            model_proto, "bias", enforce=False
+        )
+        weights = []
+        for weight in weights_data:
+            dimentions = weight.dims
+            assert weight is not None
+            if weight.data_type != 1:  # FLOATS
+                raise ValueError(
+                    "Neural Network Weights must be of type FLOATS, found other."
+                )
+            weight = weight.raw_data
+            # decode bytes object
+            weight = struct.unpack("f" * (dimentions[0] * dimentions[1]), weight)
+            weight = np.asarray(weight)
+            weight = weight.reshape(dimentions[0], dimentions[1]).T
+            weights.append(weight)
+
+        biases = []
+        for bias in biases_data:
+            dimentions = bias.dims
+            assert bias is not None
+            if bias.data_type != 1:  # FLOATS
+                raise ValueError(
+                    "Neural network biases must be of type FLOATS, found other."
+                )
+            bias = bias.raw_data
+            bias = struct.unpack("f" * dimentions[0], bias)
+            bias = np.asarray(bias)
+            biases.append(bias)
+
+        return cls(weights, biases, activations)
