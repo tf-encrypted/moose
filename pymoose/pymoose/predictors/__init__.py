8c1d74228fa313ea4ddc1dbec4f918c528087225
jvmncs <jvmncs@gmail.com>
2022-03-25 10:27:46 -0400

    expose MLPClassifier & MLPRegressor in pymoose.predictors

--- pymoose/pymoose/predictors/__init__.py
+++ pymoose/pymoose/predictors/__init__.py
@@ -1,5 +1,7 @@
 from pymoose.predictors.linear_predictor import LinearClassifier
 from pymoose.predictors.linear_predictor import LinearRegressor
+from pymoose.predictors.neural_net_predictor import MLPClassifier
+from pymoose.predictors.neural_net_predictor import MLPRegressor
 from pymoose.predictors.onnx_convert import from_onnx
 from pymoose.predictors.tree_ensemble import TreeEnsembleClassifier
 from pymoose.predictors.tree_ensemble import TreeEnsembleRegressor
@@ -8,6 +10,8 @@ __all__ = [
     "from_onnx",
     "LinearClassifier",
     "LinearRegressor",
+    "MLPClassifier",
+    "MLPRegressor",
     "TreeEnsembleClassifier",
     "TreeEnsembleRegressor",
 ]
