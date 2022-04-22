4d8b0568e2f3320115556a63882898b9b821e95d
yanndupis <yann.dupis@gmail.com>
2022-04-21 18:26:16 -0700

    test edsl reshape

--- pymoose/rust_integration_tests/reshape_test.py
+++ pymoose/rust_integration_tests/reshape_test.py
@@ -17,19 +17,33 @@ rep = edsl.replicated_placement(name="rep", players=[alice, bob, carole])
 
 
 class ReshapeExample(parameterized.TestCase):
-    def _setup_comp(self, dtype, input_placement, reshape_placement):
+    def _setup_rep_comp(self):
         @edsl.computation
         def my_comp():
-            with input_placement:
-                x = edsl.constant(np.array([[1.0, 2.0], [3.0, 4.0]]), dtype=dtype)
-                # shape = edsl.constant(np.array([[1., 1., 1., 1.]]), dtype=dtype)
+            with bob:
+                x = edsl.constant(
+                    np.array([[1.0, 2.0], [3.0, 4.0]]), dtype=edsl.fixed(8, 27)
+                )
 
-            with reshape_placement:
-                # shape = edsl.shape(shape)
-                # x_reshape = edsl.reshape(x, shape)
+            with rep:
                 x_reshape = edsl.reshape(x, [1, 4])
 
-            with input_placement:
+            with bob:
+                x_reshape = edsl.cast(x_reshape, dtype=edsl.float64)
+                res = edsl.save("x_reshape", x_reshape)
+
+            return res
+
+        return my_comp
+
+    def _setup_host_comp(self):
+        @edsl.computation
+        def my_comp():
+            with bob:
+                x = edsl.constant(
+                    np.array([[1.0, 2.0], [3.0, 4.0]]), dtype=edsl.float64
+                )
+                x_reshape = edsl.reshape(x, [1, 4])
                 res = edsl.save("x_reshape", x_reshape)
 
             return res
@@ -37,14 +51,15 @@ class ReshapeExample(parameterized.TestCase):
         return my_comp
 
     @parameterized.parameters(
-        (edsl.float64, bob),
-        # (edsl.fixed(14, 23), bob),
-        # (edsl.fixed(24, 40), bob),
-        # (edsl.fixed(14, 23), rep),
-        # (edsl.fixed(24, 40), rep),
+        (bob),
+        (rep),
     )
-    def test_example_execute(self, dtype, shape_placement):
-        comp = self._setup_comp(dtype, alice, shape_placement)
+    def test_example_execute(self, reshape_placement):
+        if reshape_placement == bob:
+            comp = self._setup_host_comp()
+        elif reshape_placement == rep:
+            comp = self._setup_rep_comp()
+
         traced_comp = edsl.trace(comp)
 
         storage = {
@@ -54,12 +69,12 @@ class ReshapeExample(parameterized.TestCase):
         }
 
         runtime = LocalMooseRuntime(storage_mapping=storage)
-        results = runtime.evaluate_computation(
+        runtime.evaluate_computation(
             computation=traced_comp,
             role_assignment={"alice": "alice", "bob": "bob", "carole": "carole"},
             arguments={},
         )
-        res_array = runtime.read_value_from_storage("alice", "x_reshape")
+        res_array = runtime.read_value_from_storage("bob", "x_reshape")
 
         assert res_array.shape == (1, 4)
 
