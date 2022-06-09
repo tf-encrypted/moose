970694dbacc95ad809b6e1990baf6fa708f98fd2
Morten Dahl <mortendahlcs@gmail.com>
2022-06-09 18:38:08 +0200

    first step

--- moose/build.rs
+++ moose/build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rustc-link-search=native={}", "/opt/homebrew/opt/openblas/lib");
     tonic_build::compile_protos("protos/choreography.proto")?;
     tonic_build::compile_protos("protos/networking.proto")?;
     Ok(())
