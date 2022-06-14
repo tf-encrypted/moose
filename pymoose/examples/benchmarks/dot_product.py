39e02cb9ebdab2cfbb974b6ff4522d2ff0bc3e54
Dragos Rotaru <r.dragos0@gmail.com>
2022-06-14 13:03:57 +0300

    added possibility to increase throughput

--- pymoose/examples/benchmarks/dot_product.py
+++ pymoose/examples/benchmarks/dot_product.py
@@ -1,10 +1,8 @@
 import argparse
-import logging
 
 import numpy as np
 
 import pymoose as pm
-from pymoose.logger import get_logger
 
 alice = pm.host_placement("alice")
 bob = pm.host_placement("bob")
@@ -13,24 +11,28 @@ carole = pm.host_placement("carole")
 rep = pm.replicated_placement(name="rep", players=[alice, bob, carole])
 
 
-@pm.computation
-def dot_product_comp(
-    x_arg: pm.Argument(placement=alice, vtype=pm.TensorType(pm.float64)),
-    y_arg: pm.Argument(placement=bob, vtype=pm.TensorType(pm.float64)),
-):
-    with alice:
-        x = pm.cast(x_arg, dtype=pm.fixed(8, 27))
+def setup_dot_computation(n_parallel):
+    @pm.computation
+    def dot_product_comp(
+        x_arg: pm.Argument(placement=alice, vtype=pm.TensorType(pm.float64)),
+        y_arg: pm.Argument(placement=bob, vtype=pm.TensorType(pm.float64)),
+    ):
+        with alice:
+            x = pm.cast(x_arg, dtype=pm.fixed(8, 27))
 
-    with bob:
-        y = pm.cast(y_arg, dtype=pm.fixed(8, 27))
+        with bob:
+            y = pm.cast(y_arg, dtype=pm.fixed(8, 27))
 
-    with rep:
-        z = pm.dot(x, y)
+        with rep:
+            z_dots = [pm.dot(x, y) for _ in range(n_parallel)]
+            z = pm.add_n(z_dots)
 
-    with carole:
-        res = pm.cast(z, pm.float64)
+        with carole:
+            res = pm.cast(z, pm.float64)
 
-    return res
+        return res
+
+    return dot_product_comp
 
 
 if __name__ == "__main__":
@@ -47,23 +49,30 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
+        "--n_parallel",
+        dest="n_parallel",
+        type=int,
+        default="1",
+        help="number of dot products in parallel for averaging the experiment",
+    )
+
+    parser.add_argument(
         "--n",
         dest="n_iter",
         type=int,
         default="1",
         help="number of iterations for averaging the experiment",
     )
+
     args = parser.parse_args()
     shape = args.shape
     n_iter = args.n_iter
+    n_parallel = args.n_parallel
 
     if isinstance(shape, list) and len(shape) > 2:
         raise ValueError(
             f"Tensor shape expects at most a 2D tensor, found shape {shape}"
         )
-
-    if args.verbose:
-        get_logger().setLevel(level=logging.DEBUG)
 
     role_map = {
         alice: "localhost:50000",
@@ -74,10 +83,13 @@ if __name__ == "__main__":
     runtime = pm.GrpcMooseRuntime(role_map)
     runtime.set_default()
 
+    dot_product_comp = setup_dot_computation(n_parallel)
+
     x = np.ones(shape, dtype=np.float64)
     y = np.ones(shape, dtype=np.float64)
 
     AVG_TIME = 0
+
     for _ in range(n_iter):
         outputs, timings = runtime.evaluate_computation(
             computation=dot_product_comp, arguments={"x_arg": x, "y_arg": y}
